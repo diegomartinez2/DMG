b972f7d2a52741760b354c383fdf3006253520da
diegomartinez2 <diegomartinez2@yahoo.es>
2022-05-10 15:58:17 +0200

    Multiprocess starmap example.

---
+++ starmap_example.py
@@ -0,0 +1,12 @@
+from multiprocessing.dummy import Pool as ThreadPool 
+
+def write(i, x):
+    print(i, "---", x)
+
+a = ["1","2","3"]
+b = ["4","5","6"] 
+
+pool = ThreadPool(2)
+pool.starmap(write, zip(a,b)) 
+pool.close() 
+pool.join()
