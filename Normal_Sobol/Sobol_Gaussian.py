68213c9ae26f08502829beefd991df36905889bd
diegomartinez2 <diegomartinez2@yahoo.es>
2022-05-17 14:29:01 +0200

    Objectification

--- Normal_Sobol/Sobol_Gaussian.py
+++ Normal_Sobol/Sobol_Gaussian.py
@@ -1,85 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  untitled.py
+#
+#  Copyright 2022 Diego <diego@u038025>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+# ---------------------------
+# Importación de los módulos
+# ---------------------------
 import numpy as np
 from scipy.stats import qmc
 import matplotlib.pyplot as plt
+# -------
+# Clases
+# -------
+class Random_generador(object):
+    def __init__(self,size):
+        self.size=size
+        ###########################################################
+    def sobol_normal(self):
+        sampler = qmc.Sobol(d=2, scramble=False)
+        size_sobol = int(np.log(self.size)/np.log(2))+1
+        print("size = ",self.size," ;size Sobol = ",size_sobol)
+        sample = sampler.random_base2(m=size_sobol)
 
-# #rand = np.random.normal(size = (size, n_modes))
-# #sampler = qmc.Sobol(d=n_modes, scramble=True)
-# size=500
-# sampler = qmc.Sobol(d=1, scramble=True)
-# size_sobol = int(np.log(size)/np.log(2))+1
-# sample = sampler.random_base2(m=size_sobol)
-#
-# data=[]
-# while (len(data)<size):
-#  data1 = sampler.random()
-#  data2 = sampler.random()
-#  v1=2.0*data1-1.0
-#  v2=2.0*data2-1.0
-# # Riq=np.linalg.norm(v1)**2+np.linalg.norm(v2)**2
-#  Riq=v1**2+v2**2
-# # Riq_n=np.linalg.norm(Riq)
-# # if(Riq_n <= 1 and Riq_n >= 0):
-#  if(0 <= Riq <= 1):
-#   data3=np.sqrt(-2*np.log(Riq)/Riq)
-#   data.append(v1[0][0]*data3[0][0])
-#
-# plt.hist(data, bins=50)
-# plt.show()
+        data1=[]
+        while (len(data1)<self.size):
+         data = sampler.random()
+         v1=2.0*data[0][0]-1.0
+         v2=2.0*data[0][1]-1.0
+         Riq=v1**2+v2**2
+         if(0 <= Riq <= 1):
+             #print("Riq=",Riq)
+             data3=np.sqrt(-2*np.log(Riq)/Riq)
+             #print("data=",v1*data3)
+             data1.append(v1*data3)
 
-###########################################
-size=100
-sampler = qmc.Sobol(d=2, scramble=False)
-size_sobol = int(np.log(size)/np.log(2))+1
-print("size = ",size," ;size Sobol = ",size_sobol)
-sample = sampler.random_base2(m=size_sobol)
+        plt.hist(data1, bins=50)
+        print("Sobol graphics")
+        plt.show()
+        plt.savefig('Sobol_hist.png')
+        plt.scatter(data1,range(len(data1)))
+        print("Sobol graphics")
+        plt.show()
+        plt.savefig('Sobol_scatter.png')
+        return data1
+        ###########################################################
+    def random_normal(self):
+        data1=[]
+        while (len(data1)<self.size):
+         v1=2.0*np.random.rand()-1.0
+         v2=2.0*np.random.rand()-1.0
+         Riq=v1**2+v2**2
+         if(0 <= Riq <= 1):
+             #print("Riq=",Riq)
+             data3=np.sqrt(-2*np.log(Riq)/Riq)
+             #print("data=",v1*data3)
+             data1.append(v1*data3)
 
-data1=[]
-while (len(data1)<size):
- data = sampler.random()
- v1=2.0*data[0][0]-1.0
- v2=2.0*data[0][1]-1.0
-# Riq=np.linalg.norm(v1)**2+np.linalg.norm(v2)**2
- Riq=v1**2+v2**2
-# Riq_n=np.linalg.norm(Riq)
-# if(Riq_n <= 1 and Riq_n >= 0):
- if(0 <= Riq <= 1):
-     #print("Riq=",Riq)
-     data3=np.sqrt(-2*np.log(Riq)/Riq)
-     #print("data=",v1*data3)
-     data1.append(v1*data3)
+        plt.hist(data1, bins=50)
+        print("random normal graphics")
+        plt.show()
+        plt.savefig('random_hist.png')
+        plt.scatter(data1,range(len(data1)))
+        print("random normal graphics")
+        plt.show()
+        plt.savefig('random_scatter.png')
+        return data1
+        ###########################################################
+    def random_numpy_normal(self):
+        data1=[]
+        while (len(data1)<self.size):
+             data1.append(np.random.normal())
+        plt.hist(data1, bins=50)
+        print("numpy normal graphics")
+        plt.show()
+        plt.savefig('numpy_normal_hist.png')
+        plt.scatter(data1,range(len(data1)))
+        print("numpy normal graphics")
+        plt.show()
+        plt.savefig('numpy_normal_scatter.png')
+        return data1
+        ###########################################################
 
-plt.hist(data1, bins=50)
-print("Sobol graphics")
-plt.show()
-plt.scatter(data1,range(len(data1)))
-print("Sobol graphics")
-plt.show()
-###########################################################
-data1=[]
-while (len(data1)<size):
- v1=2.0*np.random.rand()-1.0
- v2=2.0*np.random.rand()-1.0
- Riq=v1**2+v2**2
- if(0 <= Riq <= 1):
-     #print("Riq=",Riq)
-     data3=np.sqrt(-2*np.log(Riq)/Riq)
-     #print("data=",v1*data3)
-     data1.append(v1*data3)
+def main(args):
+    calculos = Random_generador(100)
+    calculos.sobol_normal()
+    calculos.random_normal()
+    calculos.random_numpy_normal()
+    return 0
 
-plt.hist(data1, bins=50)
-print("random normal graphics")
-plt.show()
-plt.scatter(data1,range(len(data1)))
-print("random normal graphics")
-plt.show()
-
-data1=[]
-while (len(data1)<size):
-     data1.append(np.random.normal())
-
-plt.hist(data1, bins=50)
-print("numpy normal graphics")
-plt.show()
-plt.scatter(data1,range(len(data1)))
-print("numpy normal graphics")
-plt.show()
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
