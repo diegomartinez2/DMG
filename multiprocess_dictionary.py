aef63aba870cd4a9f49c883ac1c5b4a629eccd1e
diegomartinez2 <diegomartinez2@yahoo.es>
2022-05-25 15:07:29 +0200

    Testing dictionary in multiprocessing

---
+++ multiprocess_dictionary.py
@@ -0,0 +1,16 @@
+from itertools import repeat
+import multiprocessing as mp
+import os
+import pprint
+
+def f(d: dict) -> None:
+    pid = os.getpid()
+    d[pid] = "Hi, I was written by process %d" % pid
+
+if __name__ == '__main__':
+    with mp.Manager() as manager:
+        d = manager.dict()
+        with manager.Pool() as pool:
+            pool.map(f, repeat(d, 10))
+        # `d` is a DictProxy object that can be converted to dict
+        pprint.pprint(dict(d))
