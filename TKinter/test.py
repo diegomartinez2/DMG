f199ac97912e03dfc4246359bf431aa7c413ddb6
diegomartinez2 <diegomartinez2@yahoo.es>
2022-06-20 11:13:26 +0200

    TKinter button test

--- TKinter/test.py
+++ TKinter/test.py
@@ -1,6 +1,10 @@
 # importar la libreria TK
 import tkinter as tk
 from tkinter import ttk
+# nota las funciones han de situarse antes del código que las llama
+def function_click():
+    accion.configure(text="Has pusado el boton")
+    etiqueta.configure(foreground='red')
 # iniciar una ventana
 ventana = tk.Tk()
 ventana.title("python-Tkinter")
@@ -8,6 +12,15 @@ ventana.title("python-Tkinter")
 #ventana.resizable(0,0)
 # Agregar una etiqueta
 ttk.Label(ventana, text="Texto de la etiqueta").grid(column=0,row=0)
+# Otra forma de agregar etiquetas
+etiqueta=ttk.Label(ventana,text="Otra etiqueta")
+etiqueta.grid(column=0,row=1)
+
+# Agregar un Boton
+accion=ttk.Button(ventana, text="pulsa", command=function_click)
+accion.grid(column=1,row=0)
+
+
 
 # Activar la ventana
 ventana.mainloop()
