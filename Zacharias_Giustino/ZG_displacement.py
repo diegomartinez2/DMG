54fa6ecc4aff52145dbb0d31edbf2331a6033ac5
diegomartinez2 <diegomartinez2@yahoo.es>
2022-07-18 11:56:28 +0200

    This may be wrong, there must be a unitary transformation along sucessive q-points instead of this simple symmetry transformation.

--- Zacharias_Giustino/ZG_displacement.py
+++ Zacharias_Giustino/ZG_displacement.py
@@ -55,8 +55,8 @@ class ZG_displacement(object):
         by evaluating overlap matrices M between each pair of succesive q-points. We apply the transformation
         U to the later term q of the pair and repeat with a new pair of that q and the next.
         """
-        sgn_pol=np.sign(self.pols[0])
-        for i in range(len(self.pols)):
+        sgn_pol=np.sign(self.pols[0]) #This is a test, a simple transformation of the eigenmodes
+        for i in range(len(self.pols)): 
             if (sgn_pol!=np.sign(self.pols[i])):
                 #break # change the signs of self.pols[i] to match self.pols[0]
                 for j in range(len(self.pols[i])):
