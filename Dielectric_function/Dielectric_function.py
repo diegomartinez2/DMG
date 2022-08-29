0aa28618dfd4bcd34016a845a43ff0451fb870a2
diegomartinez2 <diegomartinez2@yahoo.es>
2022-08-29 11:04:49 +0200

    Plasma frequency for 'cold' electrons approximation. (in S.I.)

--- Dielectric_function/Dielectric_function.py
+++ Dielectric_function/Dielectric_function.py
@@ -182,6 +182,9 @@ class LD():
 
         plt.show()
 
+def Plasma_frequency_SI(n_e):
+    omega_p_SI=np.sqrt((n_e*1.602176462e-19)/(9.10938188e-31*8.854187813e-12))
+    return omega_p
 
 if __name__ == '__main__':
     import numpy as np
