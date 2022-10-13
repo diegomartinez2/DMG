7030276cccff2682ff0dfc0f240a665399c9606d
diegomartinez2 <diegomartinez2@yahoo.es>
2022-10-13 10:31:27 +0200

    Response function

--- Dielectric_function/Dielectric_function.py
+++ Dielectric_function/Dielectric_function.py
@@ -242,8 +242,8 @@ def dielectric_harmonic(omega, epsilon_inf, N, atom_a, atom_b, nu):
     epsilon=epsilon_inf+4*np.pi*response_function
 
 def G(a,b,omega,nu):
-    for i in range(mu):
-        G += e(a,mu)*e(b,mu)*(1/2*omega_nu(mu))*((1/(omega-omega_nu(mu)+i*nu))-(1/(omega+omega_nu(mu)+i*nu)))
+    for mu_index in range(mu):
+        G += e(a,mu_index)*e(b,mu_index)*(1/(2*omega_nu(mu_index)))*((1/(omega-omega_nu(mu_index)+1.j*nu))-(1/(omega+omega_nu(mu_index)+1.j*nu)))
     return G
 
 if __name__ == '__main__':
