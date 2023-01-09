a2f6c26cb1dbe8875de70614e24406852718c0ca
diegomartinez2 <diegomartinez2@yahoo.es>
2023-01-09 12:04:53 +0100

    Python script to filter the arXiv and get an email daily

---
+++ ArXiv_scrapper.py
@@ -0,0 +1,58 @@
+#!/usr/local/bin/python
+
+# Import stuff
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+
+# Define keywords. You can add as many as you want
+keywords = ['keyword1', 'keyword2', 'keyword3']
+
+# Define website to be read and read it correctly
+link = "https://arxiv.org/list/cond-mat/new"
+page = requests.get(link)
+soup = BeautifulSoup( page.content, 'html.parser')
+
+# Extract the information needed and put it in format to be checked
+titles = soup.find_all('div', {'class' : 'list-title mathjax'})
+abstracts = soup.find_all('p', {'class' : 'mathjax'})
+authors = soup.find_all('div', {'class' : 'list-authors'})
+refs = soup.find_all('a', {'title' : 'Abstract'})
+lines_titles = [title.get_text() for title in titles]
+lines_abstracts = [abstract.get_text() for abstract in abstracts]
+lines_authors = [author.get_text() for author in authors]
+lines_refs = [ref.get_text() for ref in refs]
+
+# Write filtered papers on a file in your local computer.
+# In this case /PathToWhateverYouWant/arxiv_summary.txt
+filetosend = open('/PathToWhateverYouWant/arxiv_summary.txt','w')
+
+for i in range(len(lines_abstracts)):
+    if any(word in lines_abstracts[i] for word in keywords):
+        filetosend.write(lines_titles[i].encode('ascii', 'ignore').decode('ascii'))
+        filetosend.write(lines_authors[i].encode('ascii', 'ignore').decode('ascii'))
+        filetosend.write('Abstract: ')
+        filetosend.write(lines_abstracts[i].encode('ascii', 'ignore').decode('ascii'))
+        filetosend.write('https://arxiv.org/')
+        filetosend.write(lines_refs[i].encode('ascii', 'ignore'). \
+                         decode('ascii').replace('arXiv:', 'abs/'))
+        filetosend.write('\n**********\n')
+
+filetosend.close()
+
+# Prepare to send email. Read file and generate plain text.
+fp = open(filetosend, 'rb')
+msg = MIMEText(fp.read())
+fp.close()
+
+# Define Subject, From, To and send message
+# Use the same email address to send and receive
+msg['Subject'] = 'arXiv'
+msg['From'] = 'email@youruni'
+msg['To'] = 'email@youruni'
+
+# Include your SMTP server address
+s = smtplib.SMTP('smtpserver')
+s.sendmail('email@youruni', 'email@youruni', msg.as_string())
+s.quit()
