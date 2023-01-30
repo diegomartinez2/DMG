050182256bb7d76b696aff5d57a200c984acddbc
diegomartinez2 <diegomartinez2@yahoo.es>
2023-01-30 15:35:24 +0100

    Read energies fron VASP output

--- VASP/SSCHA/VaspEnergias_fuerzas.sh
+++ VASP/SSCHA/VaspEnergias_fuerzas.sh
@@ -10,7 +10,8 @@ echo -e "***********************************************************************
 echo -e "\e[0m" | awk '{printf "%15s %15s %15s\n", "Directory", "ENERGY (eV)", "VOLUME (A**3)"}'
 for d in */;
 do
-  E = 'grep "free  energy ML TOTEN  =" OUTCAR | tail -1 | awk '{printf "%f", $6 }''
+  #E = 'grep "free  energy ML TOTEN  =" OUTCAR | tail -1 | awk '{printf "%f", $6 }''
+  E = 'grep "free  energy ML TOTEN  =" $d/OUTCAR | tail -1 | awk '{printf "%f", $6 }''
 #  echo "$E" | awk '{printf "%6.6f \n ", $1* 0.0367493 } ' >> .tmp1  #Bohr units
   echo "$E" | awk '{printf "%6.6f \n ", $1* 0.0734986176 } ' >> .tmp1
   paste .tmp1 > energy
