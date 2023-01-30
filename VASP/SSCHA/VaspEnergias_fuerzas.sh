5471e875c2efc3d21ab28681ab471483015f6f0a
diegomartinez2 <diegomartinez2@yahoo.es>
2023-01-30 15:38:57 +0100

    Energies from OUTCAR vasp files?

--- VASP/SSCHA/VaspEnergias_fuerzas.sh
+++ VASP/SSCHA/VaspEnergias_fuerzas.sh
@@ -12,9 +12,13 @@ for d in */;
 do
   #E = 'grep "free  energy ML TOTEN  =" OUTCAR | tail -1 | awk '{printf "%f", $6 }''
   E = 'grep "free  energy ML TOTEN  =" $d/OUTCAR | tail -1 | awk '{printf "%f", $6 }''
+  #V=`grep "  volume of cell :" $d/OUTCAR | tail -1 | awk '{printf "%f", $5 }'
+  #echo -e "$d $E $V" | awk '{printf "%15s %15.6f %15.6f", $1, $2, $3}'
+  echo -e "$d $E" | awk '{printf "%15s %15.6f", $1, $2}'
 #  echo "$E" | awk '{printf "%6.6f \n ", $1* 0.0367493 } ' >> .tmp1  #Bohr units
   echo "$E" | awk '{printf "%6.6f \n ", $1* 0.0734986176 } ' >> .tmp1
   paste .tmp1 > energy
 
   echo -e " \e[0m"
   unset s
+done;
