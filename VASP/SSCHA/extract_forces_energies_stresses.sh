1f83f41d3389722fd9485c17d454b9082f4717a6
diegomartinez2 <diegomartinez2@yahoo.es>
2023-01-31 09:10:30 +0100

    Energies part done to test

--- VASP/SSCHA/extract_forces_energies_stresses.sh
+++ VASP/SSCHA/extract_forces_energies_stresses.sh
@@ -10,4 +10,8 @@ do
         grep -A3 'total   stress' 'population'$POPULATION'_ensemble/scf_'$i'.out' | tail -3 | awk '{print $1,$2,$3}' > 'population'$POPULATION'_ensemble/pressures_population'$POPULATION'_'$i'.dat'
 done
 
-for i in `seq 1 $N_RANDOM`; do grep ! 'population'$POPULATION'_ensemble/scf_'$i'.out' | awk '{print $5}'; done > 'population'$POPULATION'_ensemble/energies_supercell_population'$POPULATION'.dat'
+#for i in `seq 1 $N_RANDOM`; do grep ! 'population'$POPULATION'_ensemble/scf_'$i'.out' | awk '{print $5}'; done > 'population'$POPULATION'_ensemble/energies_supercell_population'$POPULATION'.dat'
+for i in `seq 1 $N_RANDOM`
+do
+  grep "free  energy ML TOTEN  =" 'population'$POPULATION'_ensemble/'$i'/OUTCAR' | tail -1 | awk '{printf "%6.6f \n ", $6 * 0.0734986176 }'
+done > 'population'$POPULATION'_ensemble/energies_supercell_population'$POPULATION'.dat'
