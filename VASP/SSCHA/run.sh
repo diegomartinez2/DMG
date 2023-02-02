f81b0813189de851bd162ac0085a0e871857cf59
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-02 07:51:51 +0100

    Creating simbolic links of the machine learning potential.

--- VASP/SSCHA/run.sh
+++ VASP/SSCHA/run.sh
@@ -2,7 +2,7 @@ N_RANDOM=`grep N_RANDOM create_configurations.py | head -1 | awk '{print $3}'`
 
 for i in `seq 1 $N_RANDOM`
 do
-#        ln -sf ./ML_FF ./${i}/ML_FF
+        ln -sf ./ML_FF ./${i}/ML_FF
         cd ${i}
         echo run${i}
         sbatch run.bash
