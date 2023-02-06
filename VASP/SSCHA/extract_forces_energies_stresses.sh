4f250ec9b340eee04f2b86d5b05f8a7f663b4cf7
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-06 15:54:34 +0100

    Fix format of pressures from XX,YY,ZZ,XY,YZ,ZZ to xx xy xz yx yy yz zx zy zz

--- VASP/SSCHA/extract_forces_energies_stresses.sh
+++ VASP/SSCHA/extract_forces_energies_stresses.sh
@@ -10,7 +10,8 @@ Angst_to_bohr=1.8897259886
 for i in `seq 1 $N_RANDOM`
 do
 #        grep force 'population'$POPULATION'_ensemble/scf_'$i'.out' | grep atom | head -${NAT} | awk '{print $7,$8,$9}' > 'population'$POPULATION'_ensemble/forces_population'$POPULATION'_'$i'.dat'
-        grep -A3 'total   stress' 'population'$POPULATION'_ensemble/'$i'/OUTCAR' | tail -3 | awk '{print $1,$2,$3}' > 'population'$POPULATION'_ensemble/pressures_population'$POPULATION'_'$i'.dat'
+#        grep -A3 'total   stress' 'population'$POPULATION'_ensemble/'$i'/OUTCAR' | tail -3 | awk '{print $1,$2,$3}' > 'population'$POPULATION'_ensemble/pressures_population'$POPULATION'_'$i'.dat'
+        grep -A3 '=-STRESS' 'population'$POPULATION'_ensemble/'$i'/OUTCAR' | tail -1 | awk '{print $2,$5,$7"\n"$5,$2,$6"\n"$7,$6,$3}' > 'population'$POPULATION'_ensemble/pressures_population'$POPULATION'_'$i'.dat'
 done
 
 #for i in `seq 1 $N_RANDOM`; do grep ! 'population'$POPULATION'_ensemble/scf_'$i'.out' | awk '{print $5}'; done > 'population'$POPULATION'_ensemble/energies_supercell_population'$POPULATION'.dat'
