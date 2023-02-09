5e89c9f3ddaa0bebeab2a1154207024749a1dd62
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-09 16:40:55 +0100

    Update

--- VASP/SSCHA/run.bash
+++ VASP/SSCHA/run.bash
@@ -36,14 +36,14 @@ IONS=54         #number of atoms in the supercells
 NCONFSSHA=300   #number of configurations in the sscha ensemble
 #mpirun vasp_std > stdout
 for i in {1..$NCONFSSHA}; do
-    echo 'date' >> timing
+    echo `date` >> timing
     cp POSCAR_$i POSCAR
     mpirun -np $np vasp_std > stdout
     grep "energy  without entropy" OUTCAR  >> energies
     grep "forces" -A $IONS vasprun.xml > forces/forces_population$POPULATION_$i.dat
     rm POSCAR
     mv OUTCAR OUTCAR_$i
-    echo 'date' >> timing
+    echo `date` >> timing
 done
 #########################################################################
 
