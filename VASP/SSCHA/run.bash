5440a90501a260e96243e8e820d3e2583cfc7b15
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-10 09:56:15 +0100

    QuickFix

--- VASP/SSCHA/run.bash
+++ VASP/SSCHA/run.bash
@@ -35,7 +35,7 @@ POPULATION=1    #population index
 IONS=54         #number of atoms in the supercells
 NCONFSSHA=300   #number of configurations in the sscha ensemble
 #mpirun vasp_std > stdout
-for i in {1..$NCONFSSHA}; do
+for i in `seq 1 $NCONFSSHA`; do
     echo `date` >> timing
     cp POSCAR_$i POSCAR
     mpirun -np $np vasp_std > stdout
