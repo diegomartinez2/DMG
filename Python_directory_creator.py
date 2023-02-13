2efd759cf8cdb3d196f7c232207def0b55acd8b5
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-13 09:37:42 +0100

    Just to remember this trick.

---
+++ Python_directory_creator.py
@@ -0,0 +1,8 @@
+# python program to check if a path exists
+#if path doesn’t exist we create a new path
+import os
+try:
+   os.makedirs("pythondirectory")
+except FileExistsError:
+   # directory already exists
+   pass
