93290554900bbaee5e1a7ab6fc5889a91e8aaed9
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-14 07:29:13 +0100

    Ciclo VASP-SSCHA

--- VASP/SSCHA/ciclo_VASP_SSCHA.sh
+++ VASP/SSCHA/ciclo_VASP_SSCHA.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 POPULATION=1
 SUPERCELL_SIZE=4
 TEMPERATURE=300
@@ -11,3 +12,10 @@ nano minimize.py
 python3 minimize.py > minim$POPULATION.out
 cd pop{&POPULATION+1}
 python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --generate $NCONFSSCHA {&POPULATION+1} $SUPERCELL_SIZE $TEMPERATURE
+cp ../POSCAR_UNITCELL POSCAR
+python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --to_vasp {&POPULATION+1} $SUPERCELL_SIZE
+cp ../INCAR.sc vasp/INCAR
+cp ../POTCAR.SrOTi vasp/POTCAR
+cp ../ML_FF vasp/ML_FF
+cp ../run.bash vasp/run.bash
+nano vasp/run.bash
