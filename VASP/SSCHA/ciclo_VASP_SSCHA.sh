24a2ad8172e45d6fbebe3173aa6ebc47e83a3170
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-14 07:39:32 +0100

    Fix and update

--- VASP/SSCHA/ciclo_VASP_SSCHA.sh
+++ VASP/SSCHA/ciclo_VASP_SSCHA.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-POPULATION=1
+POPULATION=$1
 SUPERCELL_SIZE=4
 TEMPERATURE=300
 NCONFSSCHA=512
@@ -10,10 +10,10 @@ python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --e
 cd ..
 nano minimize.py
 python3 minimize.py > minim$POPULATION.out
-cd pop{&POPULATION+1}
-python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --generate $NCONFSSCHA {&POPULATION+1} $SUPERCELL_SIZE $TEMPERATURE
+cd pop$(($POPULATION+1))
+python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --generate $NCONFSSCHA $(($POPULATION+1)) $SUPERCELL_SIZE $TEMPERATURE
 cp ../POSCAR_UNITCELL POSCAR
-python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --to_vasp {&POPULATION+1} $SUPERCELL_SIZE
+python /home/diego/github/vasp-phonopy-sscha/vasp-phonopy-sscha/interface.py --to_vasp $(($POPULATION+1)) $SUPERCELL_SIZE
 cp ../INCAR.sc vasp/INCAR
 cp ../POTCAR.SrOTi vasp/POTCAR
 cp ../ML_FF vasp/ML_FF
