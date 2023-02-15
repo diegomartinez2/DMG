8101f0396da0a6103770e7e7e41a20e4123dff36
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-15 08:49:07 +0100

    A more dinamic way of making the supercell dimmension.

--- VASP/SSCHA/Start_PHONOPY_VASP_SSCHA.sh
+++ VASP/SSCHA/Start_PHONOPY_VASP_SSCHA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[-z $1]]; then
-  echo "Use Start_PHONOPY_VASP_SSCHA 0 for the harmonic calculation
-  Use Start_PHONOPY_VASP_SSCHA 1 for the first VASP force/energy calculation"
+  echo "Use Start_PHONOPY_VASP_SSCHA 0 'N N N' for the harmonic calculation of NxNxN supercell (write it like '2 2 2' with the tildes).
+  Use Start_PHONOPY_VASP_SSCHA 1 for the first VASP force/energy calculation."
 elif  [[$1 -eq 0]]; then
 echo "SrTiO3
  3.9048
@@ -21,7 +21,8 @@ echo " IBRION = 6
  ML_FF_ISTART = 2" > INCAR.harmonic
 echo " ML_FF_LMLFF = .TRUE.
  ML_FF_ISTART = 2" > INCAR.sc
-phonopy -d --dim='4 4 4'
+#phonopy -d --dim='4 4 4'
+phonopy -d --dim=$2
 cp POSCAR POSCAR_UNITCELL
 cp SPOSCAR POSCAR
 cp INCAR.harmonic INCAR
