d8e3d6e04ea81a05a504797ef1a08224ac246260
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-15 15:55:47 +0100

    Add simple code example

--- Transformers/Anithing4.py
+++ Transformers/Anithing4.py
@@ -161,3 +161,16 @@ with gr.Blocks(css=css) as demo:
 
 demo.queue(concurrency_count=1)
 demo.launch()
+
+
+# from diffusers import StableDiffusionPipeline
+# import torch
+#
+# model_id = "andite/anything-v4.0"
+# pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+# pipe = pipe.to("cuda")
+#
+# prompt = "hatsune_miku"
+# image = pipe(prompt).images[0]
+#
+# image.save("./hatsune_miku.png")
