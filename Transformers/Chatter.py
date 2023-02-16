c26e2e1d00896e2e796fa850d4093c9873f3f095
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-16 08:44:33 +0100

    New class object to edit

--- Transformers/Chatter.py
+++ Transformers/Chatter.py
@@ -39,6 +39,28 @@ class NombredeClase(object):
         super(NombredeClase, self).__init__()
         self.arg = arg
 
+    def run(self):
+        for step in range(5):
+            # take user input
+            text = input(">> You:")
+            # encode the input and add end of string token
+            input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
+            # concatenate new user input with chat history (if there is)
+            bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
+            # generate a bot response
+            chat_history_ids = model.generate(
+                bot_input_ids,
+                max_length=1000,
+                do_sample=True,
+                top_p=0.95,
+                top_k=0,
+                temperature=0.75,
+                pad_token_id=tokenizer.eos_token_id
+            )
+            #print the output
+            output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+            print(f"DialoGPT: {output}")
+
 # ----------
 # Funciones
 # ----------
