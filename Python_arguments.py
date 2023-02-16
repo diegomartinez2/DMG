0cfd61dca58aa6b8103b765aff508b42f5a8ca9a
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-16 11:09:31 +0100

    Example of command line input in python

---
+++ Python_arguments.py
@@ -0,0 +1,19 @@
+import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print ('Input file is ', inputfile)
+   print ('Output file is ', outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
