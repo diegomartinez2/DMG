8de0c63a0fa0035f941eaf172ec60e0050f0d00e
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-27 14:14:12 +0100

    Set IoInfo call

--- VASP/SSCHA/minimize.py
+++ VASP/SSCHA/minimize.py
@@ -81,6 +81,10 @@ def sscha_run(POPULATION=1, N_RANDOM=100, SUPERCELL= (2,2,2), T=50, NQIRR=10):
     # Let's start the minimization
 
     minimizer.init()
+
+    ioinfo=sscha.Utilities.IOInfo()
+    ioinfo.SetupSaving("minim_info")
+
     minimizer.run()
 
     # Let's make some plot on the evolution of the minimization
