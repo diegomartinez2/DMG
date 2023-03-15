f661fb8ea344aa06f61498bbd99017a97cec69e2
diegomartinez2 <diegomartinez2@yahoo.es>
2023-03-15 09:21:27 +0100

    New run alone

--- VASP/SSCHA/run.sh
+++ VASP/SSCHA/run.sh
@@ -1,10 +1,22 @@
-N_RANDOM=`grep N_RANDOM create_configurations.py | head -1 | awk '{print $3}'`
+#!/bin/bash
 
-for i in `seq 1 $N_RANDOM`
-do
-        ln -sf ./ML_FF ./${i}/ML_FF
-        cd ${i}
-        echo run${i}
-        sbatch run.bash
-        cd ..
+np=4           #number of cpus
+POPULATION=1    #population index
+IONS=40         #number of atoms in the supercells
+NCONFSSHA=128   #number of configurations in the sscha ensemble
+#mpirun vasp_std > stdout
+
+for i in `seq 1 $NCONFSSHA`; do
+    echo "--run--" $i
+    echo `date` >> timing
+    cp POSCAR_$i POSCAR
+#    mpirun -np $np ~/VASP/vasp.6.3.0/bin/vasp_std > stdout
+    ~/VASP/vasp.6.3.0/bin/vasp_std > stdout
+    grep "energy  without entropy" OUTCAR | awk '{print $NF}' >> energies
+    #grep "energy  without entropy" OUTCAR  >> energies
+    grep "forces" -A $IONS vasprun.xml > forces/forces_population$POPULATION'_'$i.dat
+    rm POSCAR
+    mv OUTCAR OUTCAR_$i
+    echo `date` >> timing
 done
+#########################################################################
