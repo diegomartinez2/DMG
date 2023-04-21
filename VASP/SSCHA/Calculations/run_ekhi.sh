8ea5efc9eadd36ad14b58f6d807f8efaf2d0e6f3
diegomartinez2 <diegomartinez2@yahoo.es>
2023-04-21 09:24:48 +0200

    to test

--- VASP/SSCHA/Calculations/run_ekhi.sh
+++ VASP/SSCHA/Calculations/run_ekhi.sh
@@ -2,6 +2,7 @@ scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_
 scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
 ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
 cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
+pwd
 sbatch run.sh
 ENDSSH
 
@@ -22,7 +23,9 @@ ENDSSH
 ##########
 while True
 do
+echo "Sleep..."
 sleep 300
+echo "Awake, checking:"
 files=$(ssh diegom@ekhi.cfm.ehu.es 'squeue |grep diego')
 if [[ $? != 0 ]]; then
     echo "Command failed."
