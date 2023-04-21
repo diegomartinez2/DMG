9948d1ce2b3f0163a936a9401cf5809f1a1ca1e3
diegomartinez2 <diegomartinez2@yahoo.es>
2023-04-21 13:02:53 +0200

    Shorter ssh command

--- VASP/SSCHA/Calculations/run_ekhi.sh
+++ VASP/SSCHA/Calculations/run_ekhi.sh
@@ -1,10 +1,11 @@
 scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/
 scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
-ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
-cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
-pwd
-sbatch run.sh
-ENDSSH
+ssh -t diegom@ekhi.cfm.ehu.es 'cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp; sbatch run.sh'
+# ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
+# cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
+# pwd
+# sbatch run.sh
+# ENDSSH
 
 ##########
 while True
