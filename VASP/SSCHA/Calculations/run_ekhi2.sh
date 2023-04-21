7083c0c3955c14cfdc7ac3f4bff87d7bc4702aee
diegomartinez2 <diegomartinez2@yahoo.es>
2023-04-21 09:26:41 +0200

    Cleaning code

--- VASP/SSCHA/Calculations/run_ekhi2.sh
+++ VASP/SSCHA/Calculations/run_ekhi2.sh
@@ -1,6 +1,4 @@
 
 scp -r diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1 /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1
-scp -r diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1 /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1
-scp -r diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1 /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1
 bash run_local.sh $i
 #bash run_local.sh $(($i+1))
