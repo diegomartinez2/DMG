6aa9dbb22625097bff122ec821e7c57a07820222
diegomartinez2 <diegomartinez2@yahoo.es>
2023-04-21 09:20:57 +0200

    Fix

--- VASP/SSCHA/Calculations/run_ekhi.sh
+++ VASP/SSCHA/Calculations/run_ekhi.sh
@@ -1,28 +1,28 @@
 scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1
-scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
+scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
 ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
 cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
 sbatch run.sh
 ENDSSH
 
-scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1
-scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
-ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
-cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
-sbatch run.sh
-ENDSSH
-
-scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1
-scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
-ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
-cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
-sbatch run.sh
-ENDSSH
+# scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1
+# scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
+# ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
+# cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_100K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
+# sbatch run.sh
+# ENDSSH
+#
+# scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1
+# scp /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/run_Ekhi.bash /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp/run.sh
+# ssh diegom@ekhi.cfm.ehu.es <<-'ENDSSH'
+# cd /scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_300K_para_comparar_con_Espresso_Q4x4x4/pop$1/vasp
+# sbatch run.sh
+# ENDSSH
 
 ##########
 while True
 do
-sleep 300  
+sleep 300
 files=$(ssh diegom@ekhi.cfm.ehu.es 'squeue |grep diego')
 if [[ $? != 0 ]]; then
     echo "Command failed."
