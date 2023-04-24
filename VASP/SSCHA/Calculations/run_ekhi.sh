697b6f30ad1fa9a7c033d86e9ace95a3d42eefab
diegomartinez2 <diegomartinez2@yahoo.es>
2023-04-24 11:19:24 +0200

    Fix

--- VASP/SSCHA/Calculations/run_ekhi.sh
+++ VASP/SSCHA/Calculations/run_ekhi.sh
@@ -23,7 +23,7 @@ elif [[ $files ]]; then
     echo $files
 else
     echo "No files found."
-    scp -r ${$CLUSTER":"$DESTINO"pop$1" $ORIGEN}
+    scp -r ${$CLUSTER":"$DESTINO"pop$1"} $ORIGEN
     #scp -r diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1 /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/
 break
 fi
