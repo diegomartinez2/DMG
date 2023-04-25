c2b7433a72ea4fa10d32a1eaa53daa02a0a96859
diegomartinez2 <diegomartinez2@yahoo.es>
2023-04-25 09:43:50 +0200

    Fix bug

--- VASP/SSCHA/Calculations/run_ekhi.sh
+++ VASP/SSCHA/Calculations/run_ekhi.sh
@@ -4,7 +4,7 @@ DESTINO='/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso
 CLUSTER='diegom@ekhi.cfm.ehu.es'
 CHANGE="s/POPULATION=1/POPULATION=$1/g"
 scp -r $ORIGEN"pop$1" $CLUSTER$DESTINO
-scp ${$ORIGEN"run_Ekhi.bash"} ${$CLUSTER":"$DESTINO"run.sh"}
+scp $ORIGEN"run_Ekhi.bash" $CLUSTER":"$DESTINO"pop$1/vasp/run.sh"
 #ssh -t $CLUSTER "cd $DESTINO/pop$1/vasp;sed -i 's/POPULATION=1/POPULATION=$1/g' run.sh; sbatch run.sh"
 ssh -t $CLUSTER "cd $DESTINO/pop$1/vasp; sbatch run.sh $1"
 #scp -r /media/diego/Calculations/SrTiO3/SSCHA/Hessian_vs_T_and_Nconfs/VASP_17_4_2023/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/pop$1 diegom@ekhi.cfm.ehu.es:/scratch/diegom/SrTiO3/VASP/SrTiO3_3x3x3_50K_para_comparar_con_Espresso_Q4x4x4/
@@ -22,7 +22,7 @@ files=$(ssh $CLUSTER 'squeue |grep diego')
 if [[ $? != 0 ]]; then
     echo "No calculations running."
     echo "Taking the files from cluster"
-    scp -r ${$CLUSTER":"$DESTINO"pop$1"} $ORIGEN
+    scp -r $CLUSTER":"$DESTINO"pop$1" $ORIGEN
 elif [[ $files ]]; then
     echo $files
 else
