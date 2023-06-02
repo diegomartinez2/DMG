ace7306c55a18feb89811a1e385210b9ab9e2310
diegomartinez2 <diegomartinez2@yahoo.es>
2023-06-02 08:39:16 +0200

    test

--- Dielectric_function/Spectral_analysis/Spectral_polaron_analysis2.py
+++ Dielectric_function/Spectral_analysis/Spectral_polaron_analysis2.py
@@ -34,7 +34,7 @@ class Polaron_analysis(object):
         super(Polaron_analysis, self).__init__()
         self.arg = arg
 
-    def load_data(arg,namefile):
+    def load_data(self,namefile):
         with open(namefile) as file:
              lines = [line.rsplit() for line in file]
         Spec = np.zeros(255051)
@@ -43,7 +43,13 @@ class Polaron_analysis(object):
         data = np.resize(Spec,(51,int(len(Spec)/51)))
         return data
 
-    def plot_contour(arg,data):
+    def load_frequencies(self,namefile):
+        Frequency = np.zeros(5001)
+        for i in range(5001):
+             Frequency[i]=lines[i][2]
+        return Frequency
+
+    def plot_contour(self,data):
         plt.style.use('_mpl-gallery-nogrid')
         fig, ax1 = plt.subplots(1,1)
         cax = ax1.imshow(data.T,
@@ -57,14 +63,40 @@ class Polaron_analysis(object):
         plt.show()
         pass
 
+    def locate_1Lorenztian(self, Frequency, Spec):
+        def _1Lorentzian(x, amp1, cen1, wid1):
+            return amp1*wid1**2/((x-cen1)**2+wid1**2)
+        peaks, _ = find_peaks(Spec,width=5,rel_height=0.3)
+        print (peaks,Frequency[peaks])
+        print (peaks,_["widths"])
+        amp = 0.009
+        cen = 0.009
+        wid = 0.001
+        popt_lorentz, pcov_lorentz = scipy.optimize.curve_fit(_1Lorentzian, Frequency, Spec, p0=[amp, cen, wid])
+
+        perr_lorentz = np.sqrt(np.diag(pcov_lorentz))
+        pars = popt_lorentz[:]
+        self.lorentz_peak = _1Lorentzian(Frequency, *pars)
+        print ("-------------Peak 1-------------")
+        print ("amplitude = %0.2f (+/-) %0.2f" % (pars[0], perr_lorentz[0]))
+        print ("center = %0.2f (+/-) %0.2f" % (pars[1], perr_lorentz[1]))
+        print ("width = %0.2f (+/-) %0.2f" % (pars[2], perr_lorentz[2]))
+        print ("area = %0.2f" % np.trapz(self.lorentz_peak))
+        print ("--------------------------------")
+
+        return 0
+
 def main(arg):
     namefile = "xbw"
     print ("Creando el objeto polaron")
     polaron = Polaron_analysis(arg)
     print ("Leyendo datos")
     data = polaron.load_data(namefile)
+    frequencies = polaron.load_frequencies(namefile)
     print ("dibuja")
     polaron.plot_contour(data)
+    print("calcula ajuste loretzian")
+    polaron.locate_1Lorenztian(frequencies,data[25])
     pass
 
 if __name__ == '__main__':
