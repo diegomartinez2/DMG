c57c625429aa49d99a5947e0a9c30e796715948c
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-06 16:19:22 +0200

    Update for SrTiO3

--- SrTiO3/New_cluster/espresso_calculator.py
+++ SrTiO3/New_cluster/espresso_calculator.py
@@ -7,24 +7,31 @@ import numpy as np
 def get_calculator():
 
     pseudopotentials = {
-    "H" : "H.pbe-rrkjus_psl.1.0.0.UPF"
+    'O' : 'O.pbesol-n-kjpaw_psl.1.0.0.UPF',
+    'Sr' : 'Sr.pbesol-spn-kjpaw_psl.1.0.0.UPF',
+    'Ti' : 'Ti.pbesol-spn-kjpaw_psl.1.0.0.UPF'
+    #"H" : "H.pbe-rrkjus_psl.1.0.0.UPF"
     }
 
     imput_params = {
         "control" : {
             "tprnfor" : True,
             "tstress" : True,
-            "disk_io" : "none"
+            "disk_io" : "none" # Avoid writing wavefunctions on the disk
             },
         "system" : {
             "degauss" : 0.03,
-            "smearing" : "mv",
-            "ecutwfc" : 35,
-            "ecutrho" : 350,
-            "occupations" : "smearing"
+            #"smearing" : "mv",# remove for non-conductors and put occupations to 'fixed'
+            "ecutwfc" : 35,# Cutoff for wavefunction
+            "ecutrho" : 350,# Cutoff for the density
+            "occupations" : "fixed"# 'fixed' or 'smearing', smearing for conductors
             },
         "electrons" : {
-            "conv_thr" : 1e-10
+            #"conv_thr" : 1e-10
+            #'conv_thr' : 1e-8,
+            'conv_thr' : 1.e-09,
+            'electron_maxstep' : 80,
+            'mixing_beta' : 4.e-01
             }
     }
 
