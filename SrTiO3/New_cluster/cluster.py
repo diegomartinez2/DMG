78322dc056321c86a4bca17704695f3b8b79f027
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-06 14:55:36 +0200

    update

--- SrTiO3/New_cluster/cluster.py
+++ SrTiO3/New_cluster/cluster.py
@@ -1,3 +1,6 @@
 import cellconstructor as CC, cellconstructor.Phonons
 import sscha
 import sscha.Cluster
+import sys, os
+def configure_cluster(cluster_workdir = "SrTiO3_workdir"):
+    cluster = sscha.Cluster.Cluster(hostname = "user11@ekhi.cfm.ehu.es")
