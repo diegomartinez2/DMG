c768c90c9076ec53363fb634eb7966f752ad572f
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-10 12:21:09 +0200

    Fix simple plot

--- Dielectric_function/Spectral_analysis/Spectral_polaron_analysis2.py
+++ Dielectric_function/Spectral_analysis/Spectral_polaron_analysis2.py
@@ -251,7 +251,7 @@ class Eliashberg(object):
             print ("a2F_x=",a2F_x)
             lambda_2.append(2*np.trapz(a2F_x,dx=(Frequencies[9]-Frequencies[0])/10))
         print ("Lambda=",Lambda_1,"=",lambda_2)
-        self.plot_lambda(lambda_1)
+        self.plot_lambda(Lambda_1)
         self.plot_lambda(lambda_2)
         pass
         #return Lambda_1, lambda_2
@@ -267,8 +267,8 @@ class Eliashberg(object):
             suma += (width*center) * self.gaussian(x,center,gauss_width) #in qe-5.1.0_elph/elph_fc.f90 also has a weight related to Ne
         return factor1*suma
 
-    def plot_lambda(self,lambda):
-        plt.plot(lambda)
+    def plot_lambda(self,x):
+        plt.plot(x)
         plt.show()
         pass
 
