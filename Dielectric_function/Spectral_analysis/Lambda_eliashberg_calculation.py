aa5e38278bd0bee4fc599cc8c7b9f6b46351faf8
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-12 07:42:57 +0200

    New code for the Lambda calculation

---
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
@@ -0,0 +1,13 @@
+import Spectral_plasmons_analysis
+
+
+
+def main():
+    filelist=('xaa','xab','xac','xad','xae','xaf','xag','xah','xai','xaj','xak','xal',
+    'xam','xan','xao','xap','xaq','xar','xas','xat','xau','xav','xaw','xax','xay',
+    'xaz','xba','xbb','xbc','xbd','xbe','xbf','xbg','xbh','xbi','xbj','xbk','xbl',
+    'xbm','xbn','xbo','xbp','xbq','xbr','xbs','xbt','xbu','xbv','xbw','xbx')
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
