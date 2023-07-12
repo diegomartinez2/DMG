50023dc1b1de33bab6bae902794eb08e4b8fb73f
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-12 07:47:21 +0200

    update

--- Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
@@ -7,6 +7,14 @@ def main():
     'xam','xan','xao','xap','xaq','xar','xas','xat','xau','xav','xaw','xax','xay',
     'xaz','xba','xbb','xbc','xbd','xbe','xbf','xbg','xbh','xbi','xbj','xbk','xbl',
     'xbm','xbn','xbo','xbp','xbq','xbr','xbs','xbt','xbu','xbv','xbw','xbx')
+    pars = []
+    for i in filelist:
+        data, frequencies, qx= polaron.load_data()
+        print (np.shape(data),"=(51,5001)?")
+        polaron.fitting_Lorentz(frequencies,data)
+        np.append(pars,polaron.pars)
+
+        
 
 if __name__ == '__main__':
     import sys
