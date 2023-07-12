f312201190856b3bffe96e43050730ac297a02b4
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-12 08:30:37 +0200

    Small fix

--- Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
@@ -2,7 +2,7 @@ import Spectral_plasmons_analysis
 
 
 
-def main():
+def main(arg):
     filelist=('xaa','xab','xac','xad','xae','xaf','xag','xah','xai','xaj','xak','xal',
     'xam','xan','xao','xap','xaq','xar','xas','xat','xau','xav','xaw','xax','xay',
     'xaz','xba','xbb','xbc','xbd','xbe','xbf','xbg','xbh','xbi','xbj','xbk','xbl',
