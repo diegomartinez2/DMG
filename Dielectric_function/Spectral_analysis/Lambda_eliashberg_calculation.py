0ee68b96a74a32f8e30dc92d493d5e752c66f013
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-18 16:22:40 +0200

    test again

--- Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
@@ -52,6 +52,7 @@ def main(arg):
     for freq in Frequncies:
         gauss.append(superconductor.gaussian(freq, superconductor.pars[:,1],0.01))
     print(np.trapz(np.sum(gauss)))
+    print(np.sum(np.trapz(gauss)))
 
 if __name__ == '__main__':
     import sys
