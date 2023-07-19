a73ae67facb0fa310aa46f4cab2582a074e72005
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-19 13:56:47 +0200

    transform a2F list into numpy array to look for the shape

--- Dielectric_function/Spectral_analysis/Spectral_plasmons_analysis.py
+++ Dielectric_function/Spectral_analysis/Spectral_plasmons_analysis.py
@@ -266,7 +266,7 @@ class Eliashberg(object):
                 a2F_x.append(self.a2F_2(w)/w)
         self.lambda_2 = 2*np.trapz(a2F_x,dx=(Frequencies[9]-Frequencies[0])/10)
         print("len(freq[1:]),len(lambda1),len(lambda2),len(a2F_x) and shape of a2F_x")
-        print(len(Frequencies[1:]),len(Lambda_1),len(self.lambda_2),len(a2F_x),a2F_x.shape)
+        print(len(Frequencies[1:]),len(Lambda_1),len(self.lambda_2),len(a2F_x),np.array(a2F_x.shape))
         self.plot_lambda(Lambda_1)
         self.plot_lambda(self.lambda_2)
         lambda_2 = 2*np.trapz(self.lambda_2,dx=(Frequencies[9]-Frequencies[0])/10)
