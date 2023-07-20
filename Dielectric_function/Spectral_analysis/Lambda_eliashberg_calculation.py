Fix calculation of Lambda_q, and don't write to file (for now)

Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
@@ -39,9 +39,9 @@ def main(arg):
     superconductor.read_Ne()
     #lambda_1, lambda_2 = superconductor.Lambda(frequencies)
     lambda_1 = superconductor.Lambda(frequencies)
-    np.savetxt('Lambda.txt', (lambda_1))
+    #np.savetxt('Lambda.txt', (lambda_1))
     #print('Lambda_1=',np.sum(lambda_1)/len(lambda_1))
-    print('Lambda_1=',np.sum(lambda_1)) # Why the summation?
+    print('Lambda_1=',lambda_1) # Why the summation?
     #
     #print('Lambda_2=',np.sum(superconductor.lambda_2)*frequencies[9]-frequencies[0]/10) # "integration in w"
     print('Lambda_2=',superconductor.lambda_2)

Dielectric_function/Spectral_analysis/Spectral_plasmons_analysis.py
@@ -243,7 +243,7 @@ class Eliashberg(object):
         width = np.absolute(width)
         summa1 = 0
         for i in range(len(center)):
-            summa1 += self.Lambda_q(width,center,Ne)
+            summa1 += self.Lambda_q(width[i],center[i],Ne)
         #Lambda_1=summa1/Ne
         Lambda_1=summa1/len(center)
         self.lambda_2=[]