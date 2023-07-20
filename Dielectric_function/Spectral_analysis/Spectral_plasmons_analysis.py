5dfd5029636f4b32d88c6991d5547252abeff846
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-20 08:33:17 +0200

    Fix typo

--- Dielectric_function/Spectral_analysis/Spectral_plasmons_analysis.py
+++ Dielectric_function/Spectral_analysis/Spectral_plasmons_analysis.py
@@ -265,7 +265,7 @@ class Eliashberg(object):
             else:
                 a2F_x.append(self.a2F_2(w)/w)
         self.lambda_2 = 2*np.trapz(a2F_x,dx=(Frequencies[9]-Frequencies[0])/10)
-        print("len(freq[1:])=",len(Frequencies[1:]),",len(lambda1)=",len(Lambda_1),",lambda2=",self.lambda_2,",len(a2F_x)=",len(a2F_x)," and shape of a2F_x=",np.array(a2F_x).shape")
+        print("len(freq[1:])=",len(Frequencies[1:]),",len(lambda1)=",len(Lambda_1),",lambda2=",self.lambda_2,",len(a2F_x)=",len(a2F_x)," and shape of a2F_x=",np.array(a2F_x).shape)
         self.plot_lambda(Lambda_1)
         self.plot_lambda(self.lambda_2)
         #lambda_2 = 2*np.trapz(self.lambda_2,dx=(Frequencies[9]-Frequencies[0])/10)
