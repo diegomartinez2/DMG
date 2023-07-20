60adacd0b9a0c218d3b77bc25c3ef5f5b1240c5b
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-20 08:49:14 +0200

    Return the long list of requencies for the gaussian test

--- Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation.py
@@ -47,13 +47,13 @@ def main(arg):
     print('Lambda_2=',superconductor.lambda_2)
     #print('Lambda_2(new double integration)=',lambda_2)
     #print("len(freq[1:]),len(superconductor.lambda_2)")
-    #Frequncies = frequencies+1
-    #Frequencies = np.append(frequencies,Frequncies, axis=0)
+    Frequncies = frequencies+1
+    Frequencies = np.append(frequencies,Frequncies, axis=0)
     #print(len(Frequencies[1:]),len(superconductor.lambda_2))
     #np.savetxt('Lambda_from_a2F.txt', np.array((Frequencies[1:],superconductor.lambda_2)).T, header='frequencies,Lambda')
     print('Test Gaussian->')
     gauss = []
-    for freq in Frequencies:
+    for freq in Frequncies:
         gauss.append(superconductor.gaussian(freq, superconductor.pars[:,1],0.1))
     print(np.sum(gauss))
     print(np.sum(np.trapz(gauss)))
