bbf6098fe81bf3230e73e17e1b3699b4ea6b38b7
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-27 09:15:07 +0200

    Read DOS

--- Dielectric_function/Spectral_analysis/Lambda_test.py
+++ Dielectric_function/Spectral_analysis/Lambda_test.py
@@ -13,8 +13,8 @@ def main(arg):
     print('0--------------0')
     print(pars[:,0])
     print('Min(pars[0])=',np.amin(pars[:,0]))
-    #superconductor = Spectral_plasmons_analysis.Eliashberg(pars)
-    #superconductor.read_Ne()
+    superconductor = Spectral_plasmons_analysis.Eliashberg(pars)
+    superconductor.read_Ne()
     pass
 
 
