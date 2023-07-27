818c1cf41ff0bc7e124a8766868f5cffe480dc8f
diegomartinez2 <diegomartinez2@yahoo.es>
2023-07-27 07:03:29 +0200

    New file for testing Lambda, a2f and lambda_q

---
+++ Dielectric_function/Spectral_analysis/Lambda_test.py
@@ -0,0 +1,15 @@
+import Spectral_plasmons_analysis
+import numpy as np
+import os.path
+import matplotlib.pyplot as plt
+
+def main(arg):
+    file = 'text.txt'
+    
+    pass
+
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
