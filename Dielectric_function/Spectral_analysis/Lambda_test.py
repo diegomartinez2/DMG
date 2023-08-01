b3e1a99c083382511bbff899f1235c564e412662
diegomartinez2 <diegomartinez2@yahoo.es>
2023-08-01 09:52:50 +0200

    From Ry to Hartree

--- Dielectric_function/Spectral_analysis/Lambda_test.py
+++ Dielectric_function/Spectral_analysis/Lambda_test.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 def main(arg):
     factor_Omega = 4.55633e-6 # from cm-1 to Hartree
     factor_Gamma = 4.55633e-6/29.9793 # from GHz to Hartree
+    factor_Nef = 0.5 # from Ry to Hartree
     file = 'texto2.txt'
     if not (os.path.isfile(file)):
         print('Error, file not found')
