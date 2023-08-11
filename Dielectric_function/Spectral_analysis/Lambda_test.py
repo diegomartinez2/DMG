Fix units

Dielectric_function/Spectral_analysis/Lambda_test.py
@@ -33,8 +33,8 @@ def main(arg):
 #    print('Lambda_2=',superconductor.lambda_2) #Lambda calculated fron Eliashberg function
 #    np.savetxt('lambda_1_2.txt',(lambda_1,superconductor.lambda_2))
 #----------test--v----lambda_1---
-    center = superconductor.pars[:,1]
-    width = superconductor.pars[:,2]
+    center = superconductor.pars[:,1] * self.from_cm1_to_Hartree
+    width = superconductor.pars[:,2] * self.from_GHz_to_Hartree
     lamba = np.array([])
     for i in range(len(center)):
             lamba = np.append(lamba,superconductor.Lambda_q(width[i],center[i],Nef))