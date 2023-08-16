b929f9231cee10987dc957f5aadbde8b87ee90f3
diegomartinez2 <diegomartinez2@yahoo.es>
2023-08-16 08:10:04 +0200

    test the selection

--- Dielectric_function/Spectral_analysis/Eliashberg.py
+++ Dielectric_function/Spectral_analysis/Eliashberg.py
@@ -17,6 +17,11 @@ class Eliashberg(object):
 #        self.Ne_0[np.where(energy==0.0)]
         #numero_de_elementos = np.trapz(self.Ne[:(np.where(self.energy==0.0)[0][0])+1],dx=(self.energy[9]-self.energy[0])/10)
         numero_de_elementos = np.trapz(self.Ne[:(np.where(self.energy==0.0)[0][0]+1)],dx=np.absolute(self.energy[0]-self.energy[-1])/len(self.energy)) # maybe this is better??
+        #test the selection
+        print (self.Ne[:(np.where(self.energy==0.0)[0][0]+1)])
+        print (self.Ne[:(np.where(self.energy==0.0)[0][0])+1])
+
+        #------^--------
         print ("Density of states at Fermi level per cell=",self.Ne[np.where(self.energy==0.0)])
         print ("Number of elements (for the factor)=",numero_de_elementos)
         pass
