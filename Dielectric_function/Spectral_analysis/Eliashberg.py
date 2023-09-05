6ca0c12adb1a1d8cf19ae183eb09b14c482f1035
diegomartinez2 <diegomartinez2@yahoo.es>
2023-09-05 12:17:16 +0200

    test len(center)

--- Dielectric_function/Spectral_analysis/Eliashberg.py
+++ Dielectric_function/Spectral_analysis/Eliashberg.py
@@ -166,7 +166,7 @@ class Eliashberg(object):
         """
         center = self.pars[:,1]
         width = self.pars[:,2]
-        print('len(Center)=',len(center),'(=50*50=2500? or =51*51=2550?')
+        print('len(Center)=',len(center),'(=50*50=2500? or =50*51=2550?')
         #method 1 ----------------------------
         Nef = self.Ne[np.where(self.energy==0.0)[0][0]]  #test
         #Nef = self.Nef
