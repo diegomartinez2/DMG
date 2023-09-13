ad8dcabc838cccf7d3e1cef896b89986e9b52136
diegomartinez2 <diegomartinez2@yahoo.es>
2023-09-13 14:39:48 +0200

    test

--- Dielectric_function/Spectral_analysis/Eliashberg.py
+++ Dielectric_function/Spectral_analysis/Eliashberg.py
@@ -197,6 +197,8 @@ class Eliashberg(object):
         self.lambda_2 = self.lambda_2 = 2*np.trapz(a2F_x,dx=(Frequencies[-1]-Frequencies[0])/len(Frequencies))
         self.lambda_2_test = 2*integrate.simpson(a2F_x)
         self.lambda_2_test2 = 2*integrate.simpson(np.divide(res,w),w)
+        w = np.linspace(-100,100,20000)
+        self.lambda_2_test2 = 2*integrate.simpson(np.divide(self.a2F_new(w),w),w)
         self.plot_lambda(a2F_x)
         return Lambda_1
     def a2F_new(self,x):
