fb167bafc76a5f6cdffe481e3d581d851e60513b
diegomartinez2 <diegomartinez2@yahoo.es>
2023-09-22 09:27:07 +0200

    Fix save data

--- Dielectric_function/Spectral_analysis/Lambda_test.py
+++ Dielectric_function/Spectral_analysis/Lambda_test.py
@@ -32,7 +32,7 @@ def main(arg):
     #print('Lambda_2=',superconductor.lambda_2) #Lambda calculated fron Eliashberg function
     #print('Lambda_2_test',superconductor.lambda_2_test)
     print('Lambda_2_test2',superconductor.lambda_2_test2)
-    np.savetxt('lambda_1_2.txt',(lambda_1,superconductor.lambda_2))
+    np.savetxt('lambda_1_2.txt',(lambda_1,superconductor.lambda_2_test2))
     #print('Lambda_1/Lambda_2,Lambda_1/Lambda_2_test,Lambda_1/Lambda_2_test2:',lambda_1/superconductor.lambda_2,lambda_1/superconductor.lambda_2_test,lambda_1/superconductor.lambda_2_test2)
     #print('Lambda_1,Lambda_2*33,Lambda_2/33,Lambda_2_test*33,Lambda_2_test/33,Lambda_2_test2*33,Lambda_2_test2/33:',lambda_1,superconductor.lambda_2*33,superconductor.lambda_2/33,superconductor.lambda_2_test*33,superconductor.lambda_2_test/33,superconductor.lambda_2_test2*33,superconductor.lambda_2_test2/33)
 #----------test--v----lambda_1---
