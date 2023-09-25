e101bb5075b3416d6dd2b5f6234436a1166f7f32
diegomartinez2 <diegomartinez2@yahoo.es>
2023-09-25 08:44:13 +0200

    update

--- Dielectric_function/Spectral_analysis/Eliashberg_new.py
+++ Dielectric_function/Spectral_analysis/Eliashberg_new.py
@@ -367,7 +367,7 @@ class Eliashberg_test(object):
         width = self.pars[:,2] #*put units the same as center
         width = np.absolute(width)
         units = create_units('2014')
-        gauss_width = 5*(units.invcm/units.Hartree) #0.00002 # test the units of this... should be aprox. 5 cm-1 (1, 5 or 10)
+        gauss_width = 5*self.from_cm1_to_Hartree #0.00002 # test the units of this... should be aprox. 5 cm-1 (1, 5 or 10)
         summa = 0
         #factor1 = 1 / (2*len(center)) #a2F(w)=1/2N Sum{Lambda*Omega*delta(w-Omega)}
         factor1 = 1 / (2*np.pi*self.Nef*len(center)) #a2F(w)=1/2*pi*N_ef*N Sum{Lambda/Omega*delta(w-Omega)}
