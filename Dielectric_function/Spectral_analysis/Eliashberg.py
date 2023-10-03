7939dd57ebe9477dd9798329336865217d73e3ea
diegomartinez2 <diegomartinez2@yahoo.es>
2023-10-03 15:50:26 +0200

    Added plasmon object also here

--- Dielectric_function/Spectral_analysis/Eliashberg.py
+++ Dielectric_function/Spectral_analysis/Eliashberg.py
@@ -5,6 +5,157 @@ from ase.units import create_units
 #from scipy.integrate import quad
 from scipy import integrate
 
+import scipy as scipy
+from scipy.signal import find_peaks
+
+class Plasmon_analysis(object):
+    """Code for the analysis of Silkin's plasmon data."""
+
+    def __init__(self, arg, namefile):
+        super(Plasmon_analysis, self).__init__()
+        self.arg = arg
+        self.namefile = namefile
+        self.pars = np.zeros((51,3))
+        self.perr_lorentz = np.zeros((51,3))
+
+    def load_data(self):
+        with open(self.namefile) as file:
+             lines = [line.rsplit() for line in file]
+        Spec = np.zeros(255051)
+        for i in range(255051):
+             Spec[i]=lines[i][6]
+        #data = np.resize(Spec,(51,int(len(Spec)/51)))
+        data = np.resize(Spec,(51,5001))
+        Frequency = np.zeros(5001)
+        for i in range(5001):
+             Frequency[i]=lines[i][2]
+        print ("q_x=",lines[0][0])
+        return data, Frequency, lines[0][0]
+
+    def load_big_file(self, index, filename="A7_EPS.dat", diagonal=False):
+        q_x, q_y, w, epsilon = np.loadtxt(filename,usecols=(0,1,2,6), unpack=True)
+        print (len(np. unique(q_x))) #np. unique(my_array, return_counts=True)
+        print (len(np. unique(q_y)))
+        print (len(np. unique(w)))
+        #self.data = np.resize(epsilon,(51,51,5001))
+        self.data = np.resize(epsilon,(len(np. unique(q_x)),
+                                        len(np. unique(q_y)),
+                                        len(np. unique(w))))
+        #plot.contour(data[i])
+        """
+        direccion q_y -> data[i]
+        direccion q_x=q_y=i -> data = data[i][i] for i in {0..51}
+        direccion
+        """
+        data = np.zeros((len(np. unique(q_x)),len(np. unique(w))))
+        print ("+++++")
+        if (diagonal == True):
+            for i in range(len(np. unique(q_x))):
+                print ("[" + "=" * i +  " " * ((len(np. unique(q_x)) - i)) + "]" +  str(i) + "%",end='\r', flush=True)
+                for j in range(len(np. unique(w))):
+                    data[i,j] = self.data[i,i,j]
+
+        print ("*************")
+        print (data.shape)
+        # print (np.diagonal(self.data,axis1=0, axis2=1).T)
+        # print (np.diagonal(self.data,axis1=0, axis2=1).shape)
+        print ("--------------")
+
+        print (np.shape(self.data[int(index)]),"=(51,5001)?")
+        if (diagonal == True):
+            return data, w[:5001]
+        else:
+            return self.data[int(index)], w[:5001]
+
+    def plot_contour(self,data):
+        plt.style.use('_mpl-gallery-nogrid')
+        fig, ax1 = plt.subplots(1,1)
+        fig.set_size_inches(10, 5)
+        fig.set_dpi(100)
+        cax = ax1.imshow(data.T,
+        #	vmin = 0.0 , vmax = 0.004,
+        #	vmin = 0.0 , vmax = 0.3,
+        	cmap=plt.colormaps['jet'], origin='lower',
+        	interpolation='gaussian', aspect='auto')
+        ax1.set_ylabel(r'Frequency (cm$^{-1}$)', fontsize=12)
+
+        cbar = fig.colorbar(cax)
+        plt.show()
+        pass
+
+    def plot_contour2(self,data,data2):
+        plt.style.use('_mpl-gallery-nogrid')
+        fig, ax1 = plt.subplots(2,1)
+        fig.set_size_inches(10, 5)
+        fig.set_dpi(100)
+        cax = ax1[0].imshow(data.T,
+        #	vmin = 0.0 , vmax = 0.004,
+        #	vmin = 0.0 , vmax = 0.175,
+        	vmin = 0.0 , vmax = 0.12,
+        	cmap=plt.colormaps['jet'], origin='lower',
+        	interpolation='gaussian', aspect='auto')
+        cax2 = ax1[1].imshow(data2.T,
+        #	vmin = 0.0 , vmax = 0.004,
+        #	vmin = 0.0 , vmax = 0.3,
+        	cmap=plt.colormaps['jet'], origin='lower',
+        	interpolation='gaussian', aspect='auto')
+        #ax1[1].set_ylabel(r'Frequency (cm$^{-1}$)', fontsize=12)
+        print (self.pars[:,1])
+        cax3 = ax1[1].scatter(x = range(len(self.pars[:,1])), y = self.pars[:,1]*5000,c='k',marker='x',s=10)
+        cax3 = ax1[1].scatter(x = range(len(self.pars[:,1])), y = (self.pars[:,1]+self.pars[:,2])*5000,c='k',marker='1',s=10)
+        cax3 = ax1[1].scatter(x = range(len(self.pars[:,1])), y = (self.pars[:,1]-self.pars[:,2])*5000,c='k',marker='2',s=10)
+
+        cbar = fig.colorbar(cax)
+        cbar2 = fig.colorbar(cax2)
+        if (len( sys.argv) == 3):
+            plt.savefig("Ajuste_{}".format("original_vs_fitted"))
+        else:
+            plt.savefig("Ajuste_{}_{}".format(self.namefile,"original_vs_fitted"))
+        plt.show()
+        pass
+
+    def locate_1Lorenztian(self, Frequency, Spec, index):
+        def _1Lorentzian(x, amp1, cen1, wid1):
+            return amp1*wid1**2/((x-cen1)**2+wid1**2)
+        peaks, _ = find_peaks(Spec,width=5,rel_height=0.3)
+        print (peaks,Frequency[peaks])
+        print (peaks,_["widths"])
+        amp = 0.009
+        cen = 0.009
+        wid = 0.001
+        popt_lorentz, pcov_lorentz = scipy.optimize.curve_fit(_1Lorentzian, Frequency, Spec, p0=[amp, cen, wid])
+
+        perr_lorentz = np.sqrt(np.diag(pcov_lorentz))
+        pars = popt_lorentz[:]
+        lorentz_peak = _1Lorentzian(Frequency, *pars)
+        print ("-------------Peak ",index,"-------------")
+        print ("amplitude = %0.2f (+/-) %0.2f" % (pars[0], perr_lorentz[0]))
+        print ("center = %0.2f (+/-) %0.2f" % (pars[1], perr_lorentz[1]))
+        print ("width = %0.2f (+/-) %0.2f" % (pars[2], perr_lorentz[2]))
+        print ("area = %0.2f" % np.trapz(lorentz_peak))
+        print ("--------------------------------")
+
+        # plt.plot(Frequency, Spec, label="original")
+        # plt.plot(Frequency, lorentz_peak, "r",ls=':', label="ajuste")
+        # #plt.plot(Frequency[peaks], Spec[peaks], "xr", label="peaks")
+        # #plt.plot(Frequency[peaks], Spec[peaks], "1k", label="center")
+        # plt.legend()
+        # plt.xlim([0, Frequency.max()])
+        # plt.ylim([0, Spec.max()])
+        # plt.tight_layout()
+        # plt.savefig("Ajuste_{}_{}".format(self.namefile,index))
+        # plt.show()
+        self.pars[index] = pars
+        self.perr_lorentz[index] = perr_lorentz
+        #np.savetxt(f, (pars[0], perr_lorentz[0],pars[1], perr_lorentz[1],pars[2], perr_lorentz[2]) , fmt='%1.3f', newline=", ")
+        #f.write("\n")
+        return lorentz_peak
+
+    def fitting_Lorentz(self,frequencies,data):
+        self.Fitted_data = np.zeros((51,5001))
+        for i in range(51):
+            self.Fitted_data[i] = self.locate_1Lorenztian(frequencies,data[i],i)
+        pass
 
 class Eliashberg(object):
     """docstring for Eliashberg.
