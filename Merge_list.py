0f79bde7a990d1bb31a35a8c3797bb6dea4867dd
diegomartinez2 <diegomartinez2@yahoo.es>
2023-11-20 14:17:06 +0100

    Example of mergin and sorting in python

---
+++ Merge_list.py
@@ -0,0 +1,30 @@
+def merge(*args, fill_value = None):
+    """
+        Merge lists in python, examples:
+    merge(['a', 'b'], [1, 2], [True, False]) # [['a', 1, True], ['b', 2, False]]
+    merge(['a'], [1, 2], [True, False]) # [['a', 1, True], [None, 2, False]]
+    merge(['a'], [1, 2], [True, False], fill_value = '_')
+    # [['a', 1, True], ['_', 2, False]]
+    """
+  max_length = max([len(lst) for lst in args])
+  result = []
+  for i in range(max_length):
+    result.append([
+      args[k][i] if i < len(args[k]) else fill_value for k in range(len(args))
+    ])
+  return result
+
+
+
+
+def sort_by_indexes(lst, indexes, reverse=False):
+    """
+        Sort lists by index, examples:
+a = ['eggs', 'bread', 'oranges', 'jam', 'apples', 'milk']
+b = [3, 2, 6, 4, 1, 5]
+sort_by_indexes(a, b) # ['apples', 'bread', 'eggs', 'jam', 'milk', 'oranges']
+sort_by_indexes(a, b, True)
+# ['oranges', 'milk', 'jam', 'eggs', 'bread', 'apples']
+    """
+  return [val for (_, val) in sorted(zip(indexes, lst), key=lambda x: \
+          x[0], reverse=reverse)]
