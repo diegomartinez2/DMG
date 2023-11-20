ccb6b81ae8388a96406284eb28d8d9ad745a153c
diegomartinez2 <diegomartinez2@yahoo.es>
2023-11-20 14:21:57 +0100

    update

--- Combine_dictionaries.py
+++ Combine_dictionaries.py
@@ -13,3 +13,20 @@ def combine_values(*dicts):
     for key in d:
       res[key].append(d[key])
   return dict(res)
+
+from collections import defaultdict
+
+def collect_dictionary(obj):
+    """
+    Inverts a dictionary with non-unique hashable values. Example:
+ages = {
+  'Peter': 10,
+  'Isabel': 10,
+  'Anna': 9,
+}
+collect_dictionary(ages) # { 10: ['Peter', 'Isabel'], 9: ['Anna'] }    
+    """
+  inv_obj = defaultdict(list)
+  for key, value in obj.items():
+    inv_obj[value].append(key)
+  return dict(inv_obj)
