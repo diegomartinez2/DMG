697db1e61d9bf9b39ef3bfa89f60e9939703f2b8
diegomartinez2 <diegomartinez2@yahoo.es>
2023-11-20 14:28:31 +0100

    lists and dictionaries

---
+++ List_dictionary.py
@@ -0,0 +1,13 @@
+def list_to_dictionary(keys, values):
+    """
+    to_dictionary(['a', 'b'], [1, 2]) # { a: 1, b: 2 }
+    """
+  return dict(zip(keys, values))
+
+def dictionary_to_list(d):
+    """
+    d = {'one': 1, 'three': 3, 'five': 5, 'two': 2, 'four': 4}
+    dict_to_list(d)
+    # [('one', 1), ('three', 3), ('five', 5), ('two', 2), ('four', 4)]
+    """
+  return list(d.items())
