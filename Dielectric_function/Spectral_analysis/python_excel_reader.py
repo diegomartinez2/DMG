fbbae3ff034e18a111b6e758cc47c54f914fb5a6
diegomartinez2 <diegomartinez2@yahoo.es>
2024-02-05 15:00:46 +0100

    Fix bug

--- Dielectric_function/Spectral_analysis/python_excel_reader.py
+++ Dielectric_function/Spectral_analysis/python_excel_reader.py
@@ -2,7 +2,7 @@
 import xlrd
 
 # Open the Workbook
-workbook = xlrd.open_workbook(“sales.xlsx”)
+workbook = xlrd.open_workbook('sales.xlsx')
 
 # Open the worksheet
 worksheet = workbook.sheet_by_index(0)
@@ -19,7 +19,7 @@ print(”)
 import pandas as pd
 
 # Load the xlsx file
-excel_data = pd.read_excel(‘sales.xlsx’)
+excel_data = pd.read_excel('sales.xlsx')
 # Read the values of the file in the dataframe
 data = pd.DataFrame(excel_data, columns=[
 ‘Sales Date’, ‘Sales Person’, ‘Amount’])
