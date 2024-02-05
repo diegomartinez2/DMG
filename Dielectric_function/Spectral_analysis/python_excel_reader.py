7bc83132a776625b0dec129d7c083a8a22aa8f0c
diegomartinez2 <diegomartinez2@yahoo.es>
2024-02-05 14:57:00 +0100

    using pandas

--- Dielectric_function/Spectral_analysis/python_excel_reader.py
+++ Dielectric_function/Spectral_analysis/python_excel_reader.py
@@ -12,4 +12,16 @@ for i in range(0, 5):
 for j in range(0, 3):
 # Print the cell values with tab space
 print(worksheet.cell_value(i, j), end=’\t’)
-print(”) 
+print(”)
+
+#------------with pandas---------------
+# Import pandas
+import pandas as pd
+
+# Load the xlsx file
+excel_data = pd.read_excel(‘sales.xlsx’)
+# Read the values of the file in the dataframe
+data = pd.DataFrame(excel_data, columns=[
+‘Sales Date’, ‘Sales Person’, ‘Amount’])
+# Print the content
+print(“The content of the file is:\n”, data)
