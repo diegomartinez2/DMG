885edc8f941149cef7fb923e4151237c99368eeb
diegomartinez2 <diegomartinez2@yahoo.es>
2024-02-15 07:16:49 +0100

    Dynamic w linspace test

--- Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation2.py
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation2.py
@@ -71,7 +71,7 @@ def main(arg):
     plt.plot(gauss)
     plt.show()
     print(np.trapz(gauss))
-    w = np.linspace(-1500,1500,20000)
+    w = np.linspace(np.min(superconductor.Omega-np.abs(np.max(superconductor.Gamma))),np.max(superconductor.Omega+np.abs(np.max(superconductor.Gamma))),20000)
     #superconductor.test_gaussian(w,superconductor.pars[:,1],len(superconductor.pars[:,1]))
     #suma = integrate.simpson(superconductor.test_gaussian(w,superconductor.pars[:,1],len(superconductor.pars[:,1])), w)
     suma = integrate.simpson(superconductor.test_gaussian(w,superconductor.Omega,len(superconductor.Omega)), w)
