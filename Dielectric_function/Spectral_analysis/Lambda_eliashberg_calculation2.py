bebe48b01856425ab26c291b05bab14e1ec84bdc
diegomartinez2 <diegomartinez2@yahoo.es>
2024-02-15 08:22:45 +0100

    test

--- Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation2.py
+++ Dielectric_function/Spectral_analysis/Lambda_eliashberg_calculation2.py
@@ -7,43 +7,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 
 def main(arg):
-    #pars_create=True
-    #file1 = './pars_data.txt'
-    #file2 = './frequencies_data.txt'
-    #if pars_create:
-    #if not (os.path.isfile(file1) and os.path.isfile(file2)):
-        #print('------------------------')
-        #print('Creating parameters file')
-        #print('------------------------')
-        #filelist=('1DP','HPI','HPII')
-        #'xaa','xab','xac','xad','xae','xaf','xag','xah','xai','xaj','xak','xal',
-        #'xam','xan','xao','xap','xaq','xar','xas','xat','xau','xav','xaw','xax','xay',
-        #'xaz','xba','xbb','xbc','xbd','xbe','xbf','xbg','xbh','xbi','xbj','xbk','xbl',
-        #'xbm','xbn','xbo','xbp','xbq','xbr','xbs','xbt','xbu','xbv','xbw','xbx')
-        #for namefile in filelist:
-            #plasmon = Eliashberg.Plasmon_analysis(arg,namefile)
-            #data_all=Eliashberg.read_1_excel_file(file_directory=".",filename=namefile)
-            #data, frequencies, qx= plasmon.load_data()
-            #print (np.shape(data),"=(51,5001)?")
-            #print (np.shape(data_all))
-            #plasmon.fitting_Lorentz(frequencies,data)
-            #if (namefile == filelist[0]):
-                #pars = plasmon.pars
-                #print('pars_1=',pars)
-            #else:
-                #pars = np.concatenate((pars,plasmon.pars), axis=0)
-                #print('pars_n=',pars)
-        #print (np.shape(pars),"=(2550,3)?")
-        #np.savetxt('pars_txt.dat', pars) #no negative values
-        #np.savetxt(file1, pars) #no negative values
-        #np.savetxt(file2, frequencies) #no negative values
-    #else:
-        #np.loadtxt('pars_txt.dat', pars) #no negative values
-        #pars = [] #np.zeros((2550,3))
-        #pars = np.loadtxt(file1) #no negative values,
-        #frequencies = np.loadtxt(file2) #no negative values
-    #print(pars[:,0])
-    #print('Min(pars[0])=',np.amin(pars[:,0]))
+
     out = Eliashberg.read_1_excel_file(filename="HPI_c")
     qx,qy,Omega,Gamma,Ratio = Eliashberg.Excel_data_parser(out)
     superconductor = Eliashberg.Eliashberg2(qx,qy,Omega,Gamma,Ratio)
@@ -51,18 +15,14 @@ def main(arg):
     print("Omega range:",np.min(superconductor.Omega-np.abs(np.max(superconductor.Gamma))),'::',np.max(superconductor.Omega+np.abs(np.max(superconductor.Gamma))))
     #frequencies = np.linspace(np.min(superconductor.Omega-np.abs(np.max(superconductor.Gamma))),np.max(superconductor.Omega+np.abs(np.max(superconductor.Gamma))),20000) #test
     frequencies = np.linspace(np.min(superconductor.Omega-np.abs(np.max(superconductor.Gamma))),np.max(superconductor.Omega+np.abs(np.max(superconductor.Gamma))),20000) #test
-    #lambda_1, lambda_2 = superconductor.Lambda(frequencies)
-#    lambda_1 = superconductor.Lambda(frequencies)
 
     lambda_1 = superconductor.Lambda_new(frequencies)
 
     #np.savetxt('Lambda.txt', (lambda_1))
     print('Lambda_1=',lambda_1,'[Lambda calculated from Lambda_q]?') # Lambda calculated from Lambda_q
-    #print('Lambda_1=',lambda_1*superconductor.Ne[np.where(superconductor.energy==0.0)[0][0]]) # Lambda calculated from Lambda_q#self.Ne[np.where(self.energy==0.0)[0][0]]
+    print('Lambda_1=',lambda_1*2*superconductor.Ne[np.where(superconductor.energy==0.0)[0][0]]) # Lambda calculated from Lambda_q#self.Ne[np.where(self.energy==0.0)[0][0]]
     print('Lambda_2=',superconductor.lambda_2,'[Lambda calculated fron Eliashberg function]?') #Lambda calculated fron Eliashberg function
-    # Frequncies = frequencies+1
-    # Frequencies = np.append(frequencies,Frequncies, axis=0)
-    #print(len(Frequencies[1:]),len(superconductor.lambda_2))
+
     print('Test Gaussian->')
     gauss = np.zeros(100)
     for w in range(100): #Make this from -100 to 100??
