ca5912e989648a0c2c8bf9616a5d4adaa6682cb4
diegomartinez2 <diegomartinez2@yahoo.es>
2024-03-13 08:37:09 +0100

    update

--- SrTiO3/SSCHA_cluster/run_SSCHA_ekhi.sh
+++ SrTiO3/SSCHA_cluster/run_SSCHA_ekhi.sh
@@ -11,7 +11,7 @@ echo "Sleep..."
 sleep 300
 echo "Awake, checking:"
 #files=$(ssh $CLUSTER 'squeue -u diegom -n "16384_4_50_1,16384_4_50_2,16384_4_50_3,16384_4_50_4,16384_4_50_5,16384_4_50_6,16384_4_50_7,16384_4_50_8"|grep diego')
-files=$(ssh $CLUSTER 'squeue -u diegom -n "tetragonal_2"|grep diego')
+files=$(squeue -u diegom -n "tetragonal_2"|grep diego)
 if [[ $? != 0 ]]; then
     echo "No calculations running."
     echo "Hold for a while so we are sure the files are there"
