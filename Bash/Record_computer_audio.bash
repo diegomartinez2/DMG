8c6f43392b82bff598d688d319c678bee6c06373
Diego <diegomartinez2@yahoo.es>
2024-04-01 10:05:49 +0200

    Update headers

--- Bash/Record_computer_audio.bash
+++ Bash/Record_computer_audio.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Not tested yet
 arecord -f cd -D plughw:1,0 | ffmpeg -i - -vn -ar 44100 -ac 2 -b:a 192k output.mp3
 #this should record the audio output from a linux pc computer into a 'output.mp3' file.
--- Bash/comparing_tar_files.sh
+++ Bash/comparing_tar_files.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 diff <(tar -tvf 1.tar.gz | sort) <(tar -tvf 2.tar.gz | sort)
