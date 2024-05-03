a6eba03a94e33fb2e0af8ad244a02736f6254dad
diegomartinez2 <diegomartinez2@yahoo.es>
2024-05-03 10:46:09 +0200

    Linear regression

--- Sobol/Dibuja_2.py
+++ Sobol/Dibuja_2.py
@@ -211,9 +211,59 @@ def dibuja2():
         # plt.tight_layout()
         # plt.savefig('Conf_Omeg.png')
         #plt.show()
+
+def estimate_coef(x, y):
+	# number of observations/points
+	n = np.size(x)
+
+	# mean of x and y vector
+	m_x = np.mean(x)
+	m_y = np.mean(y)
+
+	# calculating cross-deviation and deviation about x
+	SS_xy = np.sum(y*x) - n*m_y*m_x
+	SS_xx = np.sum(x*x) - n*m_x*m_x
+
+	# calculating regression coefficients
+	b_1 = SS_xy / SS_xx
+	b_0 = m_y - b_1*m_x
+
+	return (b_0, b_1)
+
+def plot_regression_line(x, y, b):
+	# plotting the actual points as scatter plot
+	plt.scatter(x, y, color = "m",
+			marker = "o", s = 30)
+
+	# predicted response vector
+	y_pred = b[0] + b[1]*x
+
+	# plotting the regression line
+	plt.plot(x, y_pred, color = "g")
+
+	# plot line at y=0
+	plt.axhline(y=0.0, color='r', linestyle='--')
+
+	# putting labels
+	plt.xlabel('T(K)')
+	plt.ylabel('$\Omega^2$ (cm-1)')
+	plt.title(r'Squared Frequencies versus Temperatures', fontsize='small')
+	plt.tight_layout()
+	plt.savefig("Linear_regression.png")
+	#plt.savefig("Ajuste_{}".format("lineal"))
+	# function to show plot
+	plt.show()
+
+
 def main(args):
     dibuja1()
     dibuja2()
+    b = estimate_coef(x, y)
+	print("Estimated coefficients:\nb_0 = {} \
+		\nb_1 = {}".format(b[0], b[1]))
+
+	# plotting regression line
+	plot_regression_line(x, y, b)
     return 0
 
 if __name__ == '__main__':
