6566e8a8ed46bcdadf7778fe9afbdfc5abd68c81
diegomartinez2 <diegomartinez2@yahoo.es>
2024-11-19 22:50:26 +0100

    update

--- GODOT/Dni_clock/Python_clock/Dni_clock.py
+++ GODOT/Dni_clock/Python_clock/Dni_clock.py
@@ -1,7 +1,7 @@
 from tkinter import
 from tkinter.ttk import
 from time import strftime
-
+from numpy import floor #the greatest integer that does not exceed x.
 
 def Earth2Dni(arg):
     delta = strftime('%H:%M:%S %p')
