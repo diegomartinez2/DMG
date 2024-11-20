dcd14be8742db643f9aeda3e5efb892c1f598f10
diegomartinez2 <diegomartinez2@yahoo.es>
2024-11-20 11:57:01 +0100

    update

--- GODOT/Dni_clock/Python_clock/Dni_clock.py
+++ GODOT/Dni_clock/Python_clock/Dni_clock.py
@@ -5,4 +5,11 @@ from numpy import floor #the greatest integer that does not exceed x.
 #int(a//1) #the value of x with the fractional portion removed.
 def Earth2Dni(arg):
     delta = strftime('%H:%M:%S %p')
+    har = floor(Millisec / MillisecPerHar)
+    if Year<0:
+        Year = -(Year - 1)
+    if Month < 3 then
+        Month = Month + 12
+        Year = Year - 1
+    end if    
     pass
