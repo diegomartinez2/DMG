b6daffb6237562bf49ab8db829ec771378198c12
diegomartinez2 <diegomartinez2@yahoo.es>
2025-01-17 12:55:16 +0100

    Function for numerical base conversion

--- GODOT/Dni_clock/Python_clock/Dni_clock.py
+++ GODOT/Dni_clock/Python_clock/Dni_clock.py
@@ -39,6 +39,24 @@ from numpy import floor #the greatest integer that does not exceed x.
 # ----------
 # Funciones
 # ----------
+
+#Conversion numerical base:
+def toDigits(n, b):
+    """Convert a positive number n to its digit representation in base b."""
+    digits = []
+    while n > 0:
+        digits.insert(0, n % b)
+        n  = n // b
+    return digits
+
+def fromDigits(digits, b):
+    """Compute the number given by digits in base b."""
+    n = 0
+    for d in digits:
+        n = b * n + d
+    return n
+
+#Conversion to CAVE timezone
 def Earth2Dni(arg):
     delta = strftime('%H:%M:%S %p')
     har = floor(Millisec / MillisecPerHar)
