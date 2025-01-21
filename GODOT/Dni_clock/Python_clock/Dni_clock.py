795f2b8dc88deed90de2c3ba2b3d4c8547e57562
diegomartinez2 <diegomartinez2@yahoo.es>
2025-01-21 19:05:02 +0100

    Base 25

--- GODOT/Dni_clock/Python_clock/Dni_clock.py
+++ GODOT/Dni_clock/Python_clock/Dni_clock.py
@@ -161,6 +161,7 @@ def main(args):
     print("Year:", Year, "Month=", Month, "Day:", Day, "Hour=",Hour,":",Minute,":",Second)
     Hahr, Vailee, Yahr, Gahrtahvo, Tahvo, Gorahn, Prorahn = Gregorian_to_Cavernian(Day, Month, Year, Hour, Minute, Second)
     print("Hahr:", Hahr, "Yahr:",Yahr, "Vailee:",Vailee,"_",VaileeDictionary[Vailee], "Gahrtahvo:", Gahrtahvo, "Tahvo:", Tahvo, "Gorahn:", Gorahn, "Prorahn:", Prorahn)
+    print("Base 25")
     print("Hahr:", toDigits(Hahr,25), "Yahr:",Yahr, "Vailee:",Vailee,"_",VaileeDictionary[Vailee], "Gahrtahvo:", Gahrtahvo, "Tahvo:", Tahvo, "Gorahn:", Gorahn, "Prorahn:", Prorahn)
     return 0
 
