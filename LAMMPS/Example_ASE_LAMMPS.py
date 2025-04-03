6f3db8f78f116744caf24bca5157fabed6915a15
diegomartinez2 <diegomartinez2@yahoo.es>
2025-04-03 19:45:06 +0200

    Example ASE+LAMMPS

---
+++ LAMMPS/Example_ASE_LAMMPS.py
@@ -0,0 +1,17 @@
+from ase import Atom, Atoms
+from ase.build import bulk
+from ase.calculators.lammpsrun import LAMMPS
+
+parameters = {'pair_style': 'eam/alloy',
+            'pair_coeff': ['* * NiAlH_jea.eam.alloy H Ni']}
+
+files = ['NiAlH_jea.eam.alloy']
+
+Ni = bulk('Ni', cubic=True)
+H = Atom('H', position=Ni.cell.diagonal()/2)
+NiH = Ni + H
+
+lammps = LAMMPS(files=files, **parameters)
+
+NiH.calc = lammps
+print("Energy ", NiH.get_potential_energy())
