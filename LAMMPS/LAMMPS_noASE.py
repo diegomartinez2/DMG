cdd026c14445685a5c0e03b3b042c5ac0e3fcab1
diegomartinez2 <diegomartinez2@yahoo.es>
2025-04-03 20:02:26 +0200

    update

--- LAMMPS/LAMMPS_noASE.py
+++ LAMMPS/LAMMPS_noASE.py
@@ -2,7 +2,7 @@ import os
 import lammps
 import numpy as np
 import pandas as pd
-
+#Here we run simulation on a methane box using a Lenard Jones potential. Let’s write a LAMMPS input file in which you fill a box with 2000 methane particles using the TraPPE potential. The syntax is the same as the one of a classic LAMMPS input.
 methane_inp = """# LAMMPS input file for Methane
 
 units real
@@ -29,9 +29,35 @@ timestep 2.0
 thermo_style multi
 thermo 200
 """
-
+#In order to run the calculations, you have to initialize the lammps object and execute LAMMPS commands through it.
 # setup the lammps object and reduce verbosity
 # lmp = lammps.lammps(cmdargs=["-log", "none", "-nocite"])
 lmp = lammps.lammps(cmdargs=["-nocite"])
 # load the initialization commands
 lmp.commands_string(methane_inp)
+#The simulations is still alive and you can for exemple, run one NVE step just to compute energy.
+lmp.commands_list([
+    "fix NVE all nve",
+    "run 0"
+])
+
+etotal_0 = lmp.get_thermo("etotal")
+print(f"E_total = {etotal_0:.2f} kcal.mol-1")
+#Let’s now run a minimization and get back energies:
+lmp.commands_string("minimize 1.0e-4 1.0e-6 1000 1000")
+etotal_min = lmp.get_thermo("etotal")
+
+print(f"E_total after minimization {etotal_min:.2f} kcal.mol-1")
+print(f"Delta E_total = {etotal_min - etotal_0:.2f} kcal.mol-1")
+#Let’s now set up a short NVT simulations.
+lmp.commands_list([
+    "velocity all create 300. 03042019 dist gaussian mom yes rot yes",
+    "fix NVT all nvt temp 300. 300. $(100.0 * dt)",
+    "dump trj all custom 10 traj.lammpstrj id type element x y z",
+    "run 1000",
+])
+#If you want to deal with the coordinates of the methane molecules you can extract them in a numpy array:
+coords = lmp.numpy.extract_atom("x")
+print(coords.shape)
+#At the end, close the LAMMPS execution:
+lmp.close()
