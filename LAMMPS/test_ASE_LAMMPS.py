0d1c7fbc2f221499984a0d40d461384b76cad9b6
diegomartinez2 <diegomartinez2@yahoo.es>
2025-04-03 19:49:58 +0200

    test ASE+LAMMPS

---
+++ LAMMPS/test_ASE_LAMMPS.py
@@ -0,0 +1,13 @@
+from ase import Atoms, Atom
+from ase.calculators.lammpsrun import LAMMPS
+
+a = [6.5, 6.5, 7.7]
+d = 2.3608
+NaCl = Atoms([Atom('Na', [0, 0, 0]),
+                Atom('Cl', [0, 0, d])],
+                cell=a, pbc=True)
+
+calc = LAMMPS(tmp_dir="/home/devonwa/tmp/lammpstmp")
+NaCl.set_calculator(calc)
+
+print(NaCl.get_stress())
