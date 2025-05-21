ba4d109e6ef3c3208bb9f4a7f7a70f5a3ae0e441
diegomartinez2 <diegomartinez2@yahoo.es>
2025-05-21 11:25:00 +0200

    test

--- LAMMPS/FromLAMMPStoVASP.py
+++ LAMMPS/FromLAMMPStoVASP.py
@@ -1,4 +1,4 @@
 #!/usr/local/bin/python
 from ase.io import read, write
-atoms = read('structure.dat', format='lammps-data', style='atomic')
+atoms = read('structure_lammps.dat', format='lammps-data', style='atomic')
 write('POSCAR', atoms, format='vasp')
