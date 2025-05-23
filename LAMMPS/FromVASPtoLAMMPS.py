56a7e84348efd5d7a591e7e9b38c90a648eb995c
diegomartinez2 <diegomartinez2@yahoo.es>
2025-05-23 13:00:41 +0200

    update

--- LAMMPS/FromVASPtoLAMMPS.py
+++ LAMMPS/FromVASPtoLAMMPS.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
 from ase.io import read, write
+from ase import Atoms
 import glob
 
 def fromVASPtoLAMMPS(arg):
@@ -8,7 +9,18 @@ def fromVASPtoLAMMPS(arg):
     write(f'{arg}.lammps', atoms, format='lammps-data', style='atomic')
     pass
 
+def fname(arg):
+    # Example: Assign atom types
+    atoms = Atoms('Si2O', positions=[[0, 0, 0], [1, 1, 1], [2, 2, 2]])
+    atoms.set_tags([1, 1, 2])  # Assign atom types (e.g., 1 for Si, 2 for O)
+    write(f'{poscar}.lammps', atoms, format='lammps-data')
+    pass
+
 if __name__ == '__main__':
+    print(atoms.get_chemical_symbols())  # Check atom types
+    print(atoms.get_positions())         # Check positions
+    print(atoms.get_initial_charges())   # Check charges, if applicable
+
     for poscar in glob.glob('POSCAR-*'):
         atoms = read(poscar, format='vasp')
         write(f'{poscar}.lammps', atoms, format='lammps-data', style='atomic')
