502e362bd2107571bd71ec12b93f654c87d9c7d9
diegomartinez2 <diegomartinez2@yahoo.es>
2025-05-23 08:58:54 +0200

    Set as library

--- LAMMPS/FromVASPtoLAMMPS.py
+++ LAMMPS/FromVASPtoLAMMPS.py
@@ -2,11 +2,13 @@
 
 from ase.io import read, write
 import glob
-for poscar in glob.glob('POSCAR-*'):
-    atoms = read(poscar, format='vasp')
-    write(f'{poscar}.lammps', atoms, format='lammps-data', style='atomic')
 
 def fromVASPtoLAMMPS(arg):
     atoms = read(arg, format='vasp')
     write(f'{arg}.lammps', atoms, format='lammps-data', style='atomic')
     pass
+
+if __name__ == '__main__':
+    for poscar in glob.glob('POSCAR-*'):
+        atoms = read(poscar, format='vasp')
+        write(f'{poscar}.lammps', atoms, format='lammps-data', style='atomic')
