1dbcd0841450214fc1eccd92b81adffe343b4846
diegomartinez2 <diegomartinez2@yahoo.es>
2025-07-15 10:41:42 +0200

    update comment on Lanczos method

--- Lancczos.py
+++ Lancczos.py
@@ -1,5 +1,5 @@
 """
-Lancczos method on python using scipy for calculating the eigenvalues and eigenvectors.
+Example of Lancczos method on python using scipy for calculating the eigenvalues and eigenvectors.
 """
    import numpy as np
    from scipy.sparse.linalg import svds
