f8c4424cf97556194d1b0ed7e418dcf078472e53
diegomartinez2 <diegomartinez2@yahoo.es>
2025-09-12 08:18:21 +0200

    Calculate the phonon lifetime on ALAMODE

---
+++ ALAMODE/Pasos_calculo_ALAMODE/9_Phonon_lifetime.bash
@@ -0,0 +1,7 @@
+analyze_phonons.py --calc tau --temp 300 si222.result > tau300K_10.dat
+echo "$ gnuplot
+gnuplot> set xrange [1:]
+gnuplot> set logscale y
+gnuplot> set xlabel "Phonon frequency (cm^{-1})"
+gnuplot> set ylabel "Phonon lifetime (ps)"
+gnuplot> plot "tau300K_10.dat" using 3:4 w p"
