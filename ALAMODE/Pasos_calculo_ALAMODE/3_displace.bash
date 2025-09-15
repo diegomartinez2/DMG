98213ec5e634da38e8f040eae46d8da0d0382a34
diegomartinez2 <diegomartinez2@yahoo.es>
2025-09-15 08:14:12 +0200

    update

--- ALAMODE/Pasos_calculo_ALAMODE/3_displace.bash
+++ ALAMODE/Pasos_calculo_ALAMODE/3_displace.bash
@@ -1,2 +1,3 @@
+#!/bin/sh
 python displace.py --LAMMPS=LAMMPS-relax.dat --mag=0.01 --prefix harm  -pf My_displacements_patterns.pattern_HARMONIC
 python displace.py --LAMMPS=LAMMPS-relax.dat --mag=0.04 --prefix cubic  -pf My_displacements_patterns.pattern_ANHARM3
