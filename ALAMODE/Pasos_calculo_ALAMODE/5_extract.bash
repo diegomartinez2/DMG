f05d8c0d29679eb4a850894282f2dbf38df28168
diegomartinez2 <diegomartinez2@yahoo.es>
2025-09-16 15:53:46 +0200

    set bash exe

--- ALAMODE/Pasos_calculo_ALAMODE/5_extract.bash
+++ ALAMODE/Pasos_calculo_ALAMODE/5_extract.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 python extract.py --LAMMPS=relax.dat XFSET.harm* > DFSET_harmonic
 python extract.py --LAMMPS=relax.dat XFSET.cubic* > DFSET_cubic
 cat DFSET_harmonic DFSET_cubic > DFSET_merged
