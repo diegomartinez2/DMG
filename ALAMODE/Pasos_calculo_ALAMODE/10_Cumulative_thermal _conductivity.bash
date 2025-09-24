f1efa370f87bcce6baa6b3c7e977244d17631d93
diegomartinez2 <diegomartinez2@yahoo.es>
2025-09-24 08:32:07 +0200

    Step 10 of the ALAMODE calculation: Thermal conductivity

---
+++ ALAMODE/Pasos_calculo_ALAMODE/10_Cumulative_thermal _conductivity.bash
@@ -0,0 +1,7 @@
+analyze_phonons.py --calc cumulative --temp 300 --length 10000:5 si222.result > cumulative_300K_10.dat
+echo "2. Dibujamos:"
+echo "$ gnuplot"
+echo "gnuplot> set logscale x"
+echo "gnuplot> set xlabel "L (nm)""
+echo "gnuplot> set ylabel "Cumulative kappa (W/mK)""
+echo "gnuplot> plot "cumulative_300K_10.dat" using 1:2 w lp""
