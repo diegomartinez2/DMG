e133d453fdf073ff896dc1335957348e26dfde1c
diegomartinez2 <diegomartinez2@yahoo.es>
2025-10-14 11:06:45 +0200

    Update

--- LAMMPS/kappa_calculation/run_kappa.sh
+++ LAMMPS/kappa_calculation/run_kappa.sh
@@ -24,13 +24,16 @@ echo "Iniciando el cálculo de Conductividad Térmica..."
 echo "Limpiando archivos de salida anteriores..."
 
 # Eliminar archivos de salida antiguos
-rm -f J0Jt.dat params.txt log.lammps
+rm -f J0Jt.dat parametros.txt log.lammps
 
 # Ejecutar LAMMPS
 # El '-in' especifica el archivo de entrada.
 echo "Ejecutando LAMMPS con $LMP_EXE..."
-$LMP_EXE -in $LAMMPS_INPUT
-
+$LMP_EXE -in $LAMMPS_INPUT -log log.lammps
+for seed in 12345 23456 34567; do
+    sed "s/12345/${seed}/g" kappa_seed.in > kappa_${seed}.in
+    $LMP_EXE -in kappa_${seed}.in -log log_${seed}.lammps
+done
 # Verificar si LAMMPS se ejecutó correctamente
 if [ $? -ne 0 ]; then
     echo "❌ Error: La simulación de LAMMPS falló. Revisar log.lammps."
