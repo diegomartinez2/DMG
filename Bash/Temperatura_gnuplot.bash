3f7be7602cea2b83c4dbc8a187004a99b9fa38b8
diegomartinez2 <diegomartinez2@yahoo.es>
2025-10-20 08:18:45 +0200

    Example of gnuplot live-graphics

---
+++ Bash/Temperatura_gnuplot.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# temp_monitor.sh
+sensors | grep 'Core' | awk '{print $3}' | sed 's/+//;s/°C//' | \
+paste <(seq 1 8) - | \
+gnuplot -p -e "
+set title 'Temperaturas CPU - $(date)';
+set yrange [0:100];
+set style fill solid;
+# set terminal png size 800,600;
+# set output 'pressure_step.png';
+# set title 'Step vs Pressure';
+plot '-' with boxes lc rgb 'orange' title '°C';
+pause 3"
