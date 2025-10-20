2e082a1f99cc75fdabcafaf53b9d957227c64561
diegomartinez2 <diegomartinez2@yahoo.es>
2025-10-20 08:19:35 +0200

    Update

--- Bash/Temperatura_gnuplot.bash
+++ Bash/Temperatura_gnuplot.bash
@@ -6,8 +6,9 @@ gnuplot -p -e "
 set title 'Temperaturas CPU - $(date)';
 set yrange [0:100];
 set style fill solid;
+plot '-' with boxes lc rgb 'orange' title '°C';
+pause 3
 # set terminal png size 800,600;
 # set output 'pressure_step.png';
 # set title 'Step vs Pressure';
-plot '-' with boxes lc rgb 'orange' title '°C';
-pause 3"
+"
