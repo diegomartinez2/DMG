c55e0c041464fa7fd78e895a77d3a3c70d927225
diegomartinez2 <diegomartinez2@yahoo.es>
2025-11-27 10:52:08 +0100

    For Groq

--- Transformers/CLIP/GPT-4o_test.py
+++ Transformers/CLIP/GPT-4o_test.py
@@ -3,8 +3,11 @@ from openai import OpenAI
 from PIL import Image
 import base64
 
-client = OpenAI(api_key="tu_api_key_aquí")  # o usa Groq gratis con clave de groq.com
-
+#client = OpenAI(api_key="tu_api_key_aquí")  # o usa Groq gratis con clave de groq.com
+client = OpenAI(
+    api_key="gsk_tu_clave_aquí",  # ¡Pégala aquí!
+    base_url="https://api.groq.com/openai/v1"  # Esto es clave para Groq
+)
 def encode_image(image_path):
     with open(image_path, "rb") as f:
         return base64.b64encode(f.read()).decode('utf-8')
