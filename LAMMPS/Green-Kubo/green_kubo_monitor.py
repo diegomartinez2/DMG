7f8365576b9cff04c4eb6351f95291c8cd824b70
diegomartinez2 <diegomartinez2@yahoo.es>
2025-12-01 08:47:46 +0100

    Fix easy bug

--- LAMMPS/Green-Kubo/green_kubo_monitor.py
+++ LAMMPS/Green-Kubo/green_kubo_monitor.py
@@ -20,7 +20,7 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #
-#"""
+"""
 Este script permite seguir la evolución de un cálculo Green-Kubo LAMMPS
 visualizando los datos a medida que se calculan.
 """
