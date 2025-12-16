e2282ebc4e7a1d5dc7518eadec53e4dc48449788
diegomartinez2 <diegomartinez2@yahoo.es>
2025-12-16 08:23:15 +0100

    Update header

--- LAMMPS/vasp_lammps_converter(2).py
+++ LAMMPS/vasp_lammps_converter(2).py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
-import numpy as np
+# -*- coding: utf-8 -*-
+#
+#  vasp_lammps_converter.py
+#
+#  Copyright 2025 Diego Martinez Gutierrez <diego.martinez@ehu.eus>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+# ---------------------------
+# Importación de los módulos
+# ---------------------------import numpy as np
 from ase.io import read, write
 from ase import Atoms
 
