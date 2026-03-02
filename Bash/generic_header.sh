a4c3d5be5f12a85daea33d3c556fd7e3382074ab
diegomartinez2 <diegomartinez2@yahoo.es>
2026-03-02 08:21:40 +0100

    Just a generic header with a link to the github repository

---
+++ Bash/generic_header.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# =============================================================================
+# Script:        NOMBRE-DEL-SCRIPT.sh
+# Autor:         NOMBRE APELLIDO <CORREO@EJEMPLO.COM>
+# Descripción:   Qué hace este script en una línea
+# Licencia:      GNU General Public License v3.0
+# Repositorio:   https://github.com/usuario/proyecto (opcional)
+# =============================================================================
