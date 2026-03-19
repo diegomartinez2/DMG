d5a9aeca890f7370f204fbbe206d9aa72f088f7f
diegomartinez2 <diegomartinez2@yahoo.es>
2026-03-19 08:16:49 +0100

    Update header

--- collatz_visualizer.py
+++ collatz_visualizer.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  collatz_visualizer.py
+#
+#  Copyright 2025 Diego <diego@u038025>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
 # -------------------------------------------------
 # file: collatz_visualizer.py
 # -------------------------------------------------
