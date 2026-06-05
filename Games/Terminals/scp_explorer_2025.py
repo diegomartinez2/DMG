2db7429fd2f458ac2c43dbda429008ae73424df0
diegomartinez2 <diegomartinez2@yahoo.es>
2026-06-05 14:07:44 +0200

    Update

--- Games/Terminals/scp_explorer_2025.py
+++ Games/Terminals/scp_explorer_2025.py
@@ -3,7 +3,7 @@
 #
 # scp_explorer_2025.py
 #
-#  Copyright 2026 Diego Martinez Gutierrez <diego.martinez@ehu.eus>
+#  Copyright 2025 Diego Martinez Gutierrez <diego.martinez@ehu.eus>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
