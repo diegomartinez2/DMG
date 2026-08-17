c6c70c7563664aa6a51d82c1af0ae8d13c8e8e0d
diegomartinez2 <diegomartinez2@yahoo.es>
2026-08-17 14:44:45 +0200

    Update header

--- Sorting_algo.py
+++ Sorting_algo.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  Sorting_algo.py
+#
+#  Copyright 2026 Diego Martinez Gutierrez <diego.martinez@ehu.eus>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
 import time
 import random
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 '''
+Sorting algorithms
 For learning reasons, for real use is better to use the ones implemented in math libraries.
 Quicksort is not stable, using it multiple times breaks previous sortings.
 '''
